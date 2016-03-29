aba5d171d114310e3546b598c1f5b13d43ec17a3
Christoph Kirsch <ckirsch@gmail.com>
2016-03-29 20:51:21 +0200

    Fixed code indentation

--- manuscript/code/iteration.c
+++ manuscript/code/iteration.c
@@ -1,15 +1,15 @@
 int x;
 
 int main() {
-	x = 0;
+  x = 0;
 
-	x = x + 1;
+  x = x + 1;
 
-	if (x == 1)
-		x = x + 1;
+  if (x == 1)
+    x = x + 1;
 
-	while (x > 0)
-		x = x - 1;
+  while (x > 0)
+    x = x - 1;
 
-	return x;
+  return x;
 }
