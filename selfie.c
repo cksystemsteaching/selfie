052f5d19a828956bd9e9124f654cbb7a40b613eb
Ben Scherer <scherer.benjamin@gmail.com>
2016-04-23 13:59:30 +0200

    right shift works

--- selfie.c
+++ selfie.c
@@ -1199,32 +1199,31 @@ int leftShift(int n, int b) {
 
 int rightShift(int n, int b) {
     // assert: b >= 0
-    // if (n >= 0 ) {
-    //     return n >> b;
-    // } else if (b < 31) {
-    //     // return (((n + 1) + INT_MAX) >> b) + (INT_MAX >> (b + 1));
-    //     return ((n + 1) + INT_MAX) / twoToThePowerOf(b) +
-    //             (INT_MAX / twoToThePowerOf(b) + 1);
-    // } else if (b == 31) {
-    //     return 1;
-    // } else {
-    //     return 0;
-    // }
+    if (n >= 0 ) {
+        return n >> b;
+    } else if (b < 31) {
+        return (((n + 1) + INT_MAX) >> b) + ((INT_MAX >> b) + 1);
+        // return ((n + 1) + INT_MAX) / twoToThePowerOf(b) + (INT_MAX / twoToThePowerOf(b) + 1);
+    } else if (b == 31) {
+        return 1;
+    } else {
+        return 0;
+    }
 
-   if (n >= 0) {
-       if (b < 31)
-           return n / twoToThePowerOf(b);
-       else
-           return 0;
-   } else if (b < 31)
-       // works even if n == INT_MIN:
-       // shift right n with msb reset and then restore msb
-       return ((n + 1) + INT_MAX) / twoToThePowerOf(b) +
-           (INT_MAX / twoToThePowerOf(b) + 1);
-   else if (b == 31)
-       return 1;
-   else
-       return 0;
+   // if (n >= 0) {
+   //     if (b < 31)
+   //         return n / twoToThePowerOf(b);
+   //     else
+   //         return 0;
+   // } else if (b < 31)
+   //     // works even if n == INT_MIN:
+   //     // shift right n with msb reset and then restore msb
+   //     return ((n + 1) + INT_MAX) / twoToThePowerOf(b) +
+   //         (INT_MAX / twoToThePowerOf(b) + 1);
+   // else if (b == 31)
+   //     return 1;
+   // else
+   //     return 0;
 }
 
 int loadCharacter(int* s, int i) {
