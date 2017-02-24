2c900c9f0394c67d4af4ee5d2963a325fec05f48
Christoph Kirsch <ckirsch@gmail.com>
2017-02-24 15:56:12 +0100

    Updated code license in the code as well [skip ci]

--- selfie.c
+++ selfie.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016, the Selfie Project authors. All rights reserved.
+// Copyright (c) 2015-2017, the Selfie Project authors. All rights reserved.
 // Please see the AUTHORS file for details. Use of this source code is
 // governed by a BSD license that can be found in the LICENSE file.
 //
