14ff0a57f66224193b3fe6337803b4c1e7ec136a
Christian Mösl <christian.moesl@live.at>
2017-10-18 13:50:09 +0200

    Fixed main function signature

--- manuscript/code/hello-world-minified.c
+++ manuscript/code/hello-world-minified.c
@@ -1,1 +1,1 @@
-uint64_t*foo;uint64_t*main(){foo="Hello World!    ";while(*foo!=0){write(1,foo,8);foo=foo+1;}}
+uint64_t*foo;uint64_t main(){foo="Hello World!    ";while(*foo!=0){write(1,foo,8);foo=foo+1;}}
