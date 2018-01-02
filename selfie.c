39d7e80a5e032b5deff272c7e9ae9b37395a8c6a
Christoph Kirsch <ckirsch@gmail.com>
2018-01-02 17:35:48 +0100

    Even prettier profiler output

--- selfie.c
+++ selfie.c
@@ -6622,11 +6622,10 @@ uint64_t* runUntilException() {
 }
 
 void printInstructionCounter(uint64_t total, uint64_t counter, uint64_t* mnemonics) {
-  print((uint64_t*) " ");
-  printInteger(counter);
-  print((uint64_t*) " ");
   print(mnemonics);
-  print((uint64_t*) " (");
+  print((uint64_t*) ": ");
+  printInteger(counter);
+  print((uint64_t*) "(");
   printFixedPointPercentage(total, counter);
   print((uint64_t*) "%)");
 }
@@ -7522,14 +7521,14 @@ uint64_t selfie_run(uint64_t machine) {
 
   disassembleOrDebug = 0;
 
-  ic = ic_lui + ic_addi + ic_add + ic_sub + ic_mul + ic_divu + ic_remu + ic_sltu + ic_ld + ic_sd + ic_beq + ic_jal + ic_jalr + ic_ecall;
-
   print(selfieName);
   print((uint64_t*) ": selfie terminating ");
   print(getName(currentContext));
   print((uint64_t*) " with exit code ");
   printInteger(signExtend(exitCode, SYSCALL_BITWIDTH));
   println();
+
+  ic = ic_lui + ic_addi + ic_add + ic_sub + ic_mul + ic_divu + ic_remu + ic_sltu + ic_ld + ic_sd + ic_beq + ic_jal + ic_jalr + ic_ecall;
 
   print(selfieName);
   print((uint64_t*) ": summary: ");
@@ -7546,34 +7545,52 @@ uint64_t selfie_run(uint64_t machine) {
     print((uint64_t*) ": profile: total,max(ratio%)@addr,2max,3max");
   println();
 
-  printPerInstructionProfile((uint64_t*) ": calls: ", calls, callsPerProcedure);
-  printPerInstructionProfile((uint64_t*) ": loops: ", iterations, iterationsPerLoop);
-  printPerInstructionProfile((uint64_t*) ": loads: ", ic_ld, loadsPerInstruction);
-  printPerInstructionProfile((uint64_t*) ": stores: ", ic_sd, storesPerInstruction);
+  printPerInstructionProfile((uint64_t*) ": calls:   ", calls, callsPerProcedure);
+  printPerInstructionProfile((uint64_t*) ": loops:   ", iterations, iterationsPerLoop);
+  printPerInstructionProfile((uint64_t*) ": loads:   ", ic_ld, loadsPerInstruction);
+  printPerInstructionProfile((uint64_t*) ": stores:  ", ic_sd, storesPerInstruction);
 
   print(selfieName);
-  print((uint64_t*) ":");
-  printInstructionCounter(ic, ic_addi, (uint64_t*) "addi");
-  printInstructionCounter(ic, ic_ld, (uint64_t*) "ld");
-  printInstructionCounter(ic, ic_sd, (uint64_t*) "sd");
-  printInstructionCounter(ic, ic_lui, (uint64_t*) "lui");
+  print((uint64_t*) ": profile: total per instruction type");
   println();
 
   print(selfieName);
-  print((uint64_t*) ":");
+  print((uint64_t*) ": init:    ");
+  printInstructionCounter(ic, ic_lui, (uint64_t*) "lui");
+  print((uint64_t*) ", ");
+  printInstructionCounter(ic, ic_addi, (uint64_t*) "addi");
+  println();
+
+  print(selfieName);
+  print((uint64_t*) ": memory:  ");
+  printInstructionCounter(ic, ic_ld, (uint64_t*) "ld");
+  print((uint64_t*) ", ");
+  printInstructionCounter(ic, ic_sd, (uint64_t*) "sd");
+  println();
+
+  print(selfieName);
+  print((uint64_t*) ": compute: ");
   printInstructionCounter(ic, ic_add, (uint64_t*) "add");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_sub, (uint64_t*) "sub");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_mul, (uint64_t*) "mul");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_divu, (uint64_t*) "divu");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_remu, (uint64_t*) "remu");
   println();
 
   print(selfieName);
-  print((uint64_t*) ":");
+  print((uint64_t*) ": control: ");
   printInstructionCounter(ic, ic_sltu, (uint64_t*) "sltu");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_beq, (uint64_t*) "beq");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_jal, (uint64_t*) "jal");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_jalr, (uint64_t*) "jalr");
+  print((uint64_t*) ", ");
   printInstructionCounter(ic, ic_ecall, (uint64_t*) "ecall");
   println();
 
