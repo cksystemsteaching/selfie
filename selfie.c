66454b17b79fa8aecaff43c6d8bbacd4a2a47d50
Christian Mösl <christian.moesl@live.at>
2018-01-28 13:17:19 +0100

    Disassembler errors are now printed on the console

--- selfie.c
+++ selfie.c
@@ -6965,6 +6965,9 @@ void decode_execute() {
   if (execute)
     throwException(EXCEPTION_UNKNOWNINSTRUCTION, 0);
   else {
+    //report the error on the console
+    outputFD = 1;
+
     print(selfieName);
     print((uint64_t*) ": unknown instruction with ");
     printBinary(opcode, 0);
