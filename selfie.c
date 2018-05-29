a59ff8da567ebf324bede64a4cb78c0e8dabfe0b
MarioStrohmeier <mario.strohmeier@stud.sbg.ac.at>
2018-05-29 14:14:58 +0200

    Multi-Line comment support (#70)
    * Multi Line Comment
    
    * added error handling for unterminated comments

--- selfie.c
+++ selfie.c
@@ -1,80 +1,82 @@
-// Copyright (c) 2015-2018, the Selfie Project authors. All rights reserved.
-// Please see the AUTHORS file for details. Use of this source code is
-// governed by a BSD license that can be found in the LICENSE file.
-//
-// Selfie is a project of the Computational Systems Group at the
-// Department of Computer Sciences of the University of Salzburg
-// in Austria. For further information and code please refer to:
-//
-// http://selfie.cs.uni-salzburg.at
-//
-// The Selfie Project provides an educational platform for teaching
-// undergraduate and graduate students the design and implementation
-// of programming languages and runtime systems. The focus is on the
-// construction of compilers, libraries, operating systems, and even
-// virtual machine monitors. The common theme is to identify and
-// resolve self-reference in systems code which is seen as the key
-// challenge when teaching systems engineering, hence the name.
-//
-// Selfie is a self-contained 64-bit, 10-KLOC C implementation of:
-//
-// 1. a self-compiling compiler called starc that compiles
-//    a tiny but still fast subset of C called C Star (C*) to
-//    a tiny and easy-to-teach subset of RISC-V called RISC-U,
-// 2. a self-executing emulator called mipster that executes
-//    RISC-U code including itself when compiled with starc,
-// 3. a self-hosting hypervisor called hypster that provides
-//    RISC-U virtual machines that can host all of selfie,
-//    that is, starc, mipster, and hypster itself,
-// 4. a prototypical symbolic execution engine called monster
-//    that executes RISC-U code symbolically,
-// 5. a simple SAT solver that reads CNF DIMACS files, and
-// 6. a tiny C* library called libcstar utilized by selfie.
-//
-// Selfie is implemented in a single (!) file and kept minimal for simplicity.
-// There is also a simple in-memory linker, a RISC-U disassembler, a profiler,
-// and a debugger with replay as well as minimal operating system support in
-// the form of RISC-V system calls built into the emulator.
-//
-// C* is a tiny Turing-complete subset of C that includes dereferencing
-// (the * operator) but excludes composite data types, bitwise and Boolean
-// operators, and many other features. There are only unsigned 64-bit
-// integers and 64-bit pointers as well as character and string literals.
-// This choice turns out to be helpful for students to understand the
-// true role of composite data types such as arrays and records.
-// Bitwise operations are implemented in libcstar using unsigned integer
-// arithmetics helping students better understand arithmetic operators.
-// C* is supposed to be close to the minimum necessary for implementing
-// a self-compiling, single-pass, recursive-descent compiler. C* can be
-// taught in one to two weeks of classes depending on student background.
-//
-// The compiler can readily be extended to compile features missing in C*
-// and to improve performance of the generated code. The compiler generates
-// RISC-U executables in ELF format that are compatible with the official
-// RISC-V toolchain. The mipster emulator can execute RISC-U executables
-// loaded from file but also from memory immediately after code generation
-// without going through the file system.
-//
-// RISC-U is a tiny Turing-complete subset of the RISC-V instruction set.
-// It only features unsigned 64-bit integer arithmetic, double-word memory,
-// and simple control-flow instructions but neither bitwise nor byte- and
-// word-level instructions. RISC-U can be taught in one week of classes.
-//
-// The emulator implements minimal operating system support that is meant
-// to be extended by students, first as part of the emulator, and then
-// ported to run on top of it, similar to an actual operating system or
-// virtual machine monitor. The fact that the emulator can execute itself
-// helps exposing the self-referential nature of that challenge. In fact,
-// selfie goes one step further by implementing microkernel functionality
-// as part of the emulator and a hypervisor that can run as part of the
-// emulator as well as on top of it, all with the same code.
-//
-// Selfie is the result of many years of teaching systems engineering.
-// The design of the compiler is inspired by the Oberon compiler of
-// Professor Niklaus Wirth from ETH Zurich. RISC-U is inspired by the
-// RISC-V community around Professor David Patterson from UC Berkeley.
-// The design of the hypervisor is inspired by microkernels of
-// Professor Jochen Liedtke from University of Karlsruhe.
+/*
+Copyright (c) 2015-2018, the Selfie Project authors. All rights reserved.
+Please see the AUTHORS file for details. Use of this source code is
+governed by a BSD license that can be found in the LICENSE file.
+
+Selfie is a project of the Computational Systems Group at the
+Department of Computer Sciences of the University of Salzburg
+in Austria. For further information and code please refer to:
+
+http://selfie.cs.uni-salzburg.at
+
+The Selfie Project provides an educational platform for teaching
+undergraduate and graduate students the design and implementation
+of programming languages and runtime systems. The focus is on the
+construction of compilers, libraries, operating systems, and even
+virtual machine monitors. The common theme is to identify and
+resolve self-reference in systems code which is seen as the key
+challenge when teaching systems engineering, hence the name.
+
+Selfie is a self-contained 64-bit, 10-KLOC C implementation of:
+
+1. a self-compiling compiler called starc that compiles
+   a tiny but still fast subset of C called C Star (C*) to
+   a tiny and easy-to-teach subset of RISC-V called RISC-U,
+2. a self-executing emulator called mipster that executes
+   RISC-U code including itself when compiled with starc,
+3. a self-hosting hypervisor called hypster that provides
+   RISC-U virtual machines that can host all of selfie,
+   that is, starc, mipster, and hypster itself,
+4. a prototypical symbolic execution engine called monster
+   that executes RISC-U code symbolically,
+5. a simple SAT solver that reads CNF DIMACS files, and
+6. a tiny C* library called libcstar utilized by selfie.
+
+Selfie is implemented in a single (!) file and kept minimal for simplicity.
+There is also a simple in-memory linker, a RISC-U disassembler, a profiler,
+and a debugger with replay as well as minimal operating system support in
+the form of RISC-V system calls built into the emulator.
+
+C* is a tiny Turing-complete subset of C that includes dereferencing
+(the * operator) but excludes composite data types, bitwise and Boolean
+operators, and many other features. There are only unsigned 64-bit
+integers and 64-bit pointers as well as character and string literals.
+This choice turns out to be helpful for students to understand the
+true role of composite data types such as arrays and records.
+Bitwise operations are implemented in libcstar using unsigned integer
+arithmetics helping students better understand arithmetic operators.
+C* is supposed to be close to the minimum necessary for implementing
+a self-compiling, single-pass, recursive-descent compiler. C* can be
+taught in one to two weeks of classes depending on student background.
+
+The compiler can readily be extended to compile features missing in C*
+and to improve performance of the generated code. The compiler generates
+RISC-U executables in ELF format that are compatible with the official
+RISC-V toolchain. The mipster emulator can execute RISC-U executables
+loaded from file but also from memory immediately after code generation
+without going through the file system.
+
+RISC-U is a tiny Turing-complete subset of the RISC-V instruction set.
+It only features unsigned 64-bit integer arithmetic, double-word memory,
+and simple control-flow instructions but neither bitwise nor byte- and
+word-level instructions. RISC-U can be taught in one week of classes.
+
+The emulator implements minimal operating system support that is meant
+to be extended by students, first as part of the emulator, and then
+ported to run on top of it, similar to an actual operating system or
+virtual machine monitor. The fact that the emulator can execute itself
+helps exposing the self-referential nature of that challenge. In fact,
+selfie goes one step further by implementing microkernel functionality
+as part of the emulator and a hypervisor that can run as part of the
+emulator as well as on top of it, all with the same code.
+
+Selfie is the result of many years of teaching systems engineering.
+The design of the compiler is inspired by the Oberon compiler of
+Professor Niklaus Wirth from ETH Zurich. RISC-U is inspired by the
+RISC-V community around Professor David Patterson from UC Berkeley.
+The design of the hypervisor is inspired by microkernels of
+Professor Jochen Liedtke from University of Karlsruhe.
+*/
 
 // *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~ *~*~
 // -----------------------------------------------------------------
@@ -2270,6 +2272,10 @@ void getCharacter() {
 
     exit(EXITCODE_IOERROR);
   }
+  
+  // keep track of line numbers for error reporting and code annotation
+  if (isCharacterNewLine())
+    lineNumber = lineNumber + 1;
 }
 
 uint64_t isCharacterNewLine() {
@@ -2291,20 +2297,26 @@ uint64_t isCharacterWhitespace() {
 }
 
 uint64_t findNextCharacter() {
-  uint64_t inComment;
+  uint64_t inSingleLineComment;
+  uint64_t inMultiLineComment;
+  uint64_t multiLineCommentStartLineNumber;
 
   // assuming we are not in a comment
-  inComment = 0;
+  inSingleLineComment = 0;
+  inMultiLineComment = 0;
+
+  // stores the beginning of the multi line comment for error reporting reasons
+  multiLineCommentStartLineNumber = 0;
 
   // read and discard all whitespace and comments until a character is found
   // that is not whitespace and does not occur in a comment, or the file ends
   while (1) {
-    if (inComment) {
+    if (inSingleLineComment) {
       getCharacter();
 
       if (isCharacterNewLine())
         // comments end with new line
-        inComment = 0;
+        inSingleLineComment = 0;
       else if (character == CHAR_EOF)
         return character;
       else
@@ -2312,11 +2324,41 @@ uint64_t findNextCharacter() {
         // line feed and carriage return are counted below
         numberOfIgnoredCharacters = numberOfIgnoredCharacters + 1;
 
-    } else if (isCharacterWhitespace()) {
-      // keep track of line numbers for error reporting and code annotation
-      if (character == CHAR_LF)
-        lineNumber = lineNumber + 1;
+    } else if (inMultiLineComment) {
+      getCharacter();
 
+      // multi line comments end with "*/"
+      if (character == CHAR_ASTERISK) {
+        getCharacter();
+
+        if (character == CHAR_SLASH) {
+          inMultiLineComment = 0;
+
+          multiLineCommentStartLineNumber = 0;
+
+          getCharacter(); // next character after "*/"
+
+          // character could be EOF
+          if (character == CHAR_EOF)
+          	return character;
+        }
+
+        // count '/' or something else as ignored character
+        numberOfIgnoredCharacters = numberOfIgnoredCharacters + 1;
+      }
+
+      // multi line comment is never terminated
+      if (character == CHAR_EOF) {
+      	printLineNumber((uint64_t*) "error", multiLineCommentStartLineNumber);
+      	print("unterminated comment");
+      	println();
+      	exit(EXITCODE_SCANNERERROR);
+      }
+
+      // one character is always ignored
+      numberOfIgnoredCharacters = numberOfIgnoredCharacters + 1;
+
+    } else if (isCharacterWhitespace()) {
       // count line feed and carriage return as ignored characters
       numberOfIgnoredCharacters = numberOfIgnoredCharacters + 1;
 
@@ -2327,15 +2369,27 @@ uint64_t findNextCharacter() {
 
       if (character == CHAR_SLASH) {
         // "//" begins a comment
-        inComment = 1;
+        inSingleLineComment = 1;
 
         // count both slashes as ignored characters as well
         numberOfIgnoredCharacters = numberOfIgnoredCharacters + 2;
 
         // count the number of comments
         numberOfComments = numberOfComments + 1;
+      } else if (character == CHAR_ASTERISK) {
+        // "/*" begins a multi line comment
+        inMultiLineComment = 1;
+
+        // save the beginning line of the comment for error reporting
+        multiLineCommentStartLineNumber = lineNumber;
+
+        // count both slash and asterisk as ignored characters as well
+        numberOfIgnoredCharacters = numberOfIgnoredCharacters + 2;
+
+        // count the number of comments
+        numberOfComments = numberOfComments + 1;
       } else {
-        // while looking for "//" we actually found '/'
+        // while looking for "//" or "/*" we actually found '/'
         symbol = SYM_DIV;
 
         return character;
@@ -9723,13 +9777,9 @@ void dimacs_findNextCharacter(uint64_t newLine) {
         numberOfComments = numberOfComments + 1;
       }
     } else if (isCharacterWhitespace()) {
-      if (isCharacterNewLine()) {
+      if (isCharacterNewLine())
         newLine = 1;
-
-        // keep track of line numbers for error reporting and code annotation
-        if (character == CHAR_LF)
-          lineNumber = lineNumber + 1;
-      } else
+      else
         newLine = 0;
 
       // count whitespace as ignored characters
