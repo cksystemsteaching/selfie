c55eed8222644830e850ff4ffe09356484ef7980
Christian Mösl <christian.moesl@live.at>
2018-11-05 17:26:00 +0100

    Implemented test for the parser of the assembler

--- grader/self.py
+++ grader/self.py
@@ -4,8 +4,10 @@ import re
 import math
 from subprocess import Popen, PIPE
 
-number_of_tests_passed = 0
-number_of_tests_failed = 0
+number_of_positive_tests_passed = 0
+number_of_positive_tests_failed = 0
+number_of_negative_tests_passed = 0
+number_of_negative_tests_failed = 0
 
 INSTRUCTIONSIZE = 4  # in bytes
 ELF_HEADER_LEN = 120
@@ -27,20 +29,35 @@ SLL_INSTRUCTION = encode_r_format(F7_SLL, F3_SLL, OP_OP)
 SRL_INSTRUCTION = encode_r_format(F7_SRL, F3_SRL, OP_OP)
 
 
-def record_result(result, msg, output, warning=None):
-  global number_of_tests_passed, number_of_tests_failed
+def print_passed(msg):
+  print("\033[92m[PASSED]\033[0m " + msg)
+
+
+def print_failed(msg, warning, output):
+  print("\033[91m[FAILED]\033[0m " + msg)
+  if warning != None:
+    print("\033[93m > " + warning + " <\033[0m")
+  print(' >> ' + output[:-1].replace('\n', '\n >> '))
+
+
+def record_result(result, msg, output, warning, should_succeed=True):
+  global number_of_positive_tests_passed, number_of_positive_tests_failed
+  global number_of_negative_tests_passed, number_of_negative_tests_failed
 
   if result == True:
-    number_of_tests_passed += 1
-
-    print("\033[92m[PASSED]\033[0m " + msg)
+    if should_succeed:
+      number_of_positive_tests_passed += 1
+      print_passed(msg)
+    else:
+      number_of_negative_tests_failed += 1
+      print_failed(msg, warning, output)
   else:
-    number_of_tests_failed += 1
-
-    print("\033[91m[FAILED]\033[0m " + msg)
-    if warning != None:
-      print("\033[93m > " + warning + " <\033[0m")
-    print(' >> ' + output[:-1].replace('\n', '\n >> '))
+    if should_succeed:
+      number_of_positive_tests_failed += 1
+      print_failed(msg, warning, output)
+    else:
+      number_of_negative_tests_passed += 1
+      print_passed(msg)
 
 
 def test_compilable(file, msg, should_succeed=True):
@@ -63,7 +80,7 @@ def test_compilable(file, msg, should_succeed=True):
       succeeded = False
       warning = match.group(0)
 
-  record_result(succeeded, msg, output, warning)
+  record_result(succeeded, msg, output, warning, should_succeed)
 
 
 def test_instruction_format(file, instruction, instruction_mask, msg):
@@ -96,32 +113,46 @@ def test_instruction_format(file, instruction, instruction_mask, msg):
   record_result(exit_code == 0, msg, output, warning)
 
 
-def test_execution(file, result, msg):
+def test_mipster_execution(file, result, msg):
   p = Popen(['./selfie', '-c', 'grader/' + file, '-m', '128'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
   output = p.stdout.read().decode(sys.stdout.encoding)
   p.wait()
 
   if p.returncode != result:
-    warning = 'Execution terminated with wrong exit code {} instead of {}'.format(p.returncode, result)
+    warning = 'Mipster execution terminated with wrong exit code {} instead of {}'.format(p.returncode, result)
   else:
     warning = None
 
   record_result(p.returncode == result, msg, output, warning)
 
 
+def test_execution(command, msg, should_succeed=True):
+  p = Popen(command.split(' '), stdout=PIPE, stderr=PIPE, stdin=PIPE)
+
+  output = p.stdout.read().decode(sys.stdout.encoding)
+  p.wait()
+
+  if should_succeed:
+    warning = f'Execution terminated with wrong exit code {p.returncode} instead of 0'
+  else:
+    warning = f'Execution terminated with wrong exit code {p.returncode}'
+
+  record_result(p.returncode == 0, msg, output, warning, should_succeed)
+
+
 def test_hex_literal():
   test_compilable('hex-integer-literal.c', 
     'hex integer literal with all characters compiled')
-  test_execution('hex-integer-literal.c', 1,
+  test_mipster_execution('hex-integer-literal.c', 1,
     'hex integer literal with all characters has the right value')
   test_compilable('hex-integer-literal-max.c',
     'maximum hex integer literal compiled')
-  test_execution('hex-integer-literal-max.c', 1,
+  test_mipster_execution('hex-integer-literal-max.c', 1,
     'maximum hex integer literal has the right value')
   test_compilable('hex-integer-literal-max.c',
     'minimum hex integer literal compiled')
-  test_execution('hex-integer-literal-max.c', 1,
+  test_mipster_execution('hex-integer-literal-max.c', 1,
     'minimum hex integer literal has the right value')
   test_compilable('hex-integer-literal-invalid.c', 
     'out of bounds hex integer literal has not compiled', should_succeed=False)
@@ -140,13 +171,13 @@ def test_shift(direction):
     'bitwise-' + direction + '-shift operator with literals compiled')
   test_instruction_format(literal_file, instruction, R_FORMAT_MASK,
     'bitwise-' + direction + '-shift operator has right RISC-V encoding')
-  test_execution(literal_file, 2,
+  test_mipster_execution(literal_file, 2,
     'bitwise-' + direction + '-shift operator calculates the right result for literals when executed with MIPSTER')
   test_compilable(variable_file,
     'bitwise-' + direction + '-shift operator with variables compiled')
   test_instruction_format(variable_file, instruction, R_FORMAT_MASK,
     'bitwise-' + direction + '-shift operator has right RISC-V encoding')
-  test_execution(variable_file, 2,
+  test_mipster_execution(variable_file, 2,
     'bitwise-' + direction + '-shift operator calculates the right result for variables when executed with MIPSTER')
 
 
@@ -159,24 +190,40 @@ def test_structs():
     'empty struct with initialization compiled')
   test_compilable('struct-member-initialization.c',
     'initialization of trivial struct members compiled')
-  test_execution('struct-member-initialization.c', 123,
+  test_mipster_execution('struct-member-initialization.c', 123,
     'read and write operations of trivial struct member works when executed with MIPSTER')
   test_compilable('struct-nested-declaration.c',
     'struct declaration with struct members compiled')
   test_compilable('struct-nested-initialization.c',
     'struct initialization with struct members compiled')
-  test_execution('struct-nested-initialization.c', 123,
+  test_mipster_execution('struct-nested-initialization.c', 123,
     'read and write operations of nested struct member works when executed with MIPSTER')
   test_compilable('struct-as-parameter.c',
     'struct as function parameter compiled')
-  test_execution('struct-as-parameter.c', 1,
+  test_mipster_execution('struct-as-parameter.c', 1,
     'read and write operations of structs as parameter work when executed with MIPSTER')
 
 
-def grade():
-  number_of_tests_passed, number_of_tests_failed
+def test_assembler(stage):
+  if stage >= 1:
+    test_execution('./selfie -c selfie.c -s selfie.s -a selfie.s',
+      'selfie can parse it\'s own disassembly files')
+    test_execution('./selfie -a grader/assembler-missing-address.s',
+      'assembly file with a missing address is not parseable', should_succeed=False)
+    test_execution('./selfie -a grader/assembler-missing-instruction.s',
+      'assembly file with a missing instruction is not parseable', should_succeed=False)
+    test_execution('./selfie -a grader/assembler-missing-literal.s',
+      'assembly file with a missing literal is not parseable', should_succeed=False)
 
-  number_of_tests = number_of_tests_passed + number_of_tests_failed
+
+def grade():
+  global number_of_positive_tests_passed, number_of_positive_tests_failed
+  global number_of_negative_tests_passed, number_of_negative_tests_failed
+
+  number_of_tests =  number_of_positive_tests_passed + number_of_positive_tests_failed
+  number_of_tests += number_of_negative_tests_passed + number_of_negative_tests_failed
+
+  number_of_tests_passed = number_of_positive_tests_passed + number_of_negative_tests_passed
 
   if number_of_tests == 0:
     print('nothing to grade')
@@ -199,7 +246,13 @@ def grade():
     grade = 5
     color = 91
 
+  if number_of_positive_tests_passed == 0:
+    print('warning:       you have not passed at least one positive test')
+    grade = 5
+    color = 91
+
   print(f'your grade is: \033[{color}m\033[1m{grade}\033[0m')
+
 
 if __name__ == "__main__":
   if len(sys.argv) <= 1:
@@ -217,6 +270,10 @@ if __name__ == "__main__":
       test_shift(direction='right')
     elif test == 'struct':
       test_structs()
+    elif re.match(r'^assembler-([1-1])$', test):
+      stage = re.search(r'^assembler-([1-1])$', test).group(1)
+
+      test_assembler(int(stage))
     else:
       print(f'unknown test: {test}')
 
