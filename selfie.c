fdd30fba09d829978c4e5800ff26b0362a44a723
Sara <sara.seidl@stud.sbg.ac.at>
2018-11-14 13:58:23 +0100

    simple fork implementation

--- selfie.c
+++ selfie.c
@@ -931,6 +931,9 @@ void     implement_open(uint64_t* context);
 void emit_malloc();
 void implement_brk(uint64_t* context);
 
+void emit_fork();
+void implement_fork(uint64_t* context);
+
 // ------------------------ GLOBAL CONSTANTS -----------------------
 
 uint64_t debug_read  = 0;
@@ -943,6 +946,7 @@ uint64_t SYSCALL_READ  = 63;
 uint64_t SYSCALL_WRITE = 64;
 uint64_t SYSCALL_OPEN  = 1024;
 uint64_t SYSCALL_BRK   = 214;
+uint64_t SYSCALL_FORK    = 402;
 
 // -----------------------------------------------------------------
 // ----------------------- HYPSTER SYSCALLS ------------------------
@@ -4778,6 +4782,7 @@ void selfie_compile() {
   emit_open();
   emit_malloc();
   emit_switch();
+  emit_fork();
 
   // implicitly declare main procedure in global symbol table
   // copy "main" string into zeroed double word to obtain unique hash
@@ -6336,6 +6341,25 @@ void implement_brk(uint64_t* context) {
 
   set_pc(context, get_pc(context) + INSTRUCTIONSIZE);
 }
+
+void emit_fork() {
+    create_symbol_table_entry(LIBRARY_TABLE, (uint64_t*) "fork", 0, PROCEDURE, UINT64_T, 0, binary_length);
+    
+    emit_addi(REG_A7, REG_ZR, SYSCALL_FORK);
+    
+    emit_ecall();
+    
+    // jump back to caller, return value is in REG_A0
+    emit_jalr(REG_ZR, REG_RA, 0);
+}
+
+
+void implement_fork(uint64_t* context) {
+    *(get_regs(context) + REG_A0) = fork();
+    
+    set_pc(context, get_pc(context) + INSTRUCTIONSIZE);
+}
+
 
 
 // -----------------------------------------------------------------
@@ -9216,6 +9240,8 @@ uint64_t handle_system_call(uint64_t* context) {
     implement_write(context);
   else if (a7 == SYSCALL_OPEN)
     implement_open(context);
+  else if (a7 == SYSCALL_FORK)
+    implement_fork(context);
   else if (a7 == SYSCALL_EXIT) {
     implement_exit(context);
 
