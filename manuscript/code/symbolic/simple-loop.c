d789907125ee48629066051cff3a708e6596f4db
ChrisEdel <christian.edelmayer@stud.sbg.ac.at>
2019-04-18 16:15:25 +0200

    added test files

--- manuscript/code/symbolic/simple-loop.c
+++ manuscript/code/symbolic/simple-loop.c
@@ -4,19 +4,24 @@ of the monster model generator of selfie. Monster translates
 the code to an SMT-LIB or BTOR2 formula that is satisfiable
 if and only if the code exits with a non-zero exit code.
 
-Solution: *x != #b00101010
+Solution: #b00101010
 */
 
 uint64_t main() {
+  uint64_t  a;
   uint64_t* x;
 
+  a = 0;
   x = malloc(8);
 
   read(1, x, 1);
 
-  if (*x > 42)
-    return 1;
-  else if (*x < 42)
+  while(*x < 50) {
+    a = a + 1;
+    *x = *x + 1;
+  }
+
+  if (a == 8)
     return 1;
   else
     return 0;
