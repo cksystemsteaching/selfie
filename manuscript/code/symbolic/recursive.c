20e12f2d65c69528ccd3c0c5a0559e8a45f75fa6
ChrisEdel <christian.edelmayer@stud.sbg.ac.at>
2019-04-27 18:32:51 +0200

    added recursive symbolic example

---
+++ manuscript/code/symbolic/recursive.c
@@ -0,0 +1,35 @@
+/*
+The purpose of this code is to demonstrate the capabilities
+of the monster model generator of selfie. Monster translates
+the code to an SMT-LIB or BTOR2 formula that is satisfiable
+if and only if the code exits with a non-zero exit code.
+
+Input == #b00110001 (== 49 == '1')
+*/
+
+uint64_t fib(uint64_t n);
+
+uint64_t main() {
+  uint64_t  a;
+  uint64_t* x;
+
+  x = malloc(8);
+  
+  read(1, x, 1);
+
+  *x = *x - 46;
+
+  a = fib(*x);
+
+  if (a == 2)
+    return 1;
+  else
+    return 0;
+}
+
+uint64_t fib(uint64_t n) {
+  if (n <= 1)
+    return n;
+  else
+    return fib(n - 1) + fib(n - 2);
+}
