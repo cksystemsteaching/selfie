830df2eae366787113d625a1aee6ba76b2b7f13b
Christoph Kirsch <ckirsch@gmail.com>
2019-05-03 10:02:16 +0200

    Updating comment header in symbolic examples, streamlining recursive example

--- manuscript/code/symbolic/recursive.c
+++ manuscript/code/symbolic/recursive.c
@@ -2,19 +2,25 @@
 The purpose of this code is to demonstrate the capabilities
 of the monster model generator of selfie. Monster translates
 the code to an SMT-LIB or BTOR2 formula that is satisfiable
-if and only if the code exits with a non-zero exit code.
+if and only if the code exits with a non-zero exit code, or
+performs division by zero or invalid/unsafe memory accesses.
 
 Input == #b00110001 (== 49 == '1')
 */
 
-uint64_t fib(uint64_t n);
+uint64_t fib(uint64_t n) {
+  if (n <= 1)
+    return n;
+  else
+    return fib(n - 1) + fib(n - 2);
+}
 
 uint64_t main() {
   uint64_t  a;
   uint64_t* x;
 
   x = malloc(8);
-  
+
   read(1, x, 1);
 
   *x = *x - 46;
@@ -25,11 +31,4 @@ uint64_t main() {
     return 1;
   else
     return 0;
-}
-
-uint64_t fib(uint64_t n) {
-  if (n <= 1)
-    return n;
-  else
-    return fib(n - 1) + fib(n - 2);
 }
