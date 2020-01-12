b0d3dd8ac847b03a9f2d6bebad63fada3391a39a
Son_Typ <Son_Typ@riapei.at>
2020-01-12 15:53:49 +0100

    Rename pid -> offset to reflect unknown scheduler behaviour

--- grader/assignments/thread/syscalls.c
+++ grader/assignments/thread/syscalls.c
@@ -4,36 +4,36 @@ uint64_t  pthread_join(uint64_t* wstatus);
 
 uint64_t* status;
 
-uint64_t sumChilds(uint64_t own_pid, uint64_t pid, uint64_t acc) {
+uint64_t sumChilds(uint64_t offset, uint64_t pid, uint64_t acc) {
   if (pid) {
     // this is the parent process/thread
     // => accumulate pid from pthread_create call, pid from pthread_join call and exit status code
-    acc = acc + pid + pthread_join(status + own_pid - 1);
-    acc = acc + *(status + own_pid - 1);
+    acc = acc + pid + pthread_join(status + offset - 1);
+    acc = acc + *(status + offset - 1);
   }
 
   return acc;
 }
 
-uint64_t get_own_pid(uint64_t pid1, uint64_t pid2, uint64_t pid3) {
-  uint64_t pid;
+uint64_t get_unique_offset(uint64_t pid1, uint64_t pid2, uint64_t pid3) {
+  uint64_t offset;
 
-  pid = 0;
+  offset = 0;
 
   if (pid1 != 0)
-    pid = pid + 1;
-  pid = pid * 2;
+    offset = offset + 1;
+  offset = offset * 2;
   if (pid2 != 0)
-    pid = pid + 1;
-  pid = pid * 2;
+    offset = offset + 1;
+  offset = offset * 2;
   if (pid3 != 0)
-    pid = pid + 1;
+    offset = offset + 1;
 
-  return 8 - pid;
+  return 8 - offset;
 } 
 
 int main(int argc, char** argv) {
-  uint64_t own_pid;
+  uint64_t offset;
   uint64_t pid1;
   uint64_t pid2;
   uint64_t pid3;
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
   pid2 = pthread_create();
   pid3 = pthread_create();
 
-  own_pid = get_own_pid(pid1, pid2, pid3);
+  offset = get_unique_offset(pid1, pid2, pid3);
 
   // do not wait for child-threads of the parent-process
   if (pid3 == 0)
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
   if (pid2 == 0)
     pid1 = 0;
 
-  result = sumChilds(own_pid, pid3, sumChilds(own_pid, pid2, sumChilds(own_pid, pid1, 0)));
+  result = sumChilds(offset, pid3, sumChilds(offset, pid2, sumChilds(offset, pid1, 0)));
 
   if (pid1 != 0)
     if (pid2 != 0)
