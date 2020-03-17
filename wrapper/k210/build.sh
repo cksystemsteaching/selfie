a24f6086fbffe19093fd49b12b2f31bb65843457
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-03-17 14:40:18 +0100

    Fix build errors because of wrong code model

--- wrapper/k210/build.sh
+++ wrapper/k210/build.sh
@@ -2,8 +2,8 @@
 
 # COMPILATION
 #riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib -c sbi_wrapper.c -I../opensbi/include -o libsbi_wrapper.o
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib -c hello-world.c -o hello_world.o -D'uint64_t = unsigned long long int'
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c hello-world.c -o hello_world.o -D'uint64_t = unsigned long long int'
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
 
 # MERGE OBJECT FILES
 riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o hello_world.o -o test.o
@@ -12,6 +12,6 @@ riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o he
 riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x08000000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.preprocessed.ld
 
 # LINKING
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.preprocessed.ld
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.preprocessed.ld
 
 riscv64-unknown-elf-objcopy -S -O binary own-payload.elf own-payload.bin
