a594da69a1099d1f08423bb592d34877aedf981e
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-03-23 14:26:04 +0100

    build_opensbi: Add qemu

--- wrapper/k210/build_opensbi.sh
+++ wrapper/k210/build_opensbi.sh
@@ -13,4 +13,5 @@ fi
 
 cd ../opensbi
 
+PLATFORM_RISCV_XLEN=64 make PLATFORM=qemu/virt O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/own-payload.bin
 PLATFORM_RISCV_XLEN=64 make PLATFORM=kendryte/k210 O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/own-payload.bin
