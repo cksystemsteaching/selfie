0b9f0bb3d8a14aa4d7afbf52e1ef6cbfb8e799a7
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-03-25 16:36:46 +0100

    Build: Add FW_PAYLOAD_OFFSET

--- wrapper/k210/build.sh
+++ wrapper/k210/build.sh
@@ -9,7 +9,7 @@ riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestand
 riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o hello_world.o -o test.o
 
 # LINKER SCRIPT PREPROCESSING
-riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x08000000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.preprocessed.ld
+riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x1A000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.preprocessed.ld
 
 # LINKING
 riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.preprocessed.ld
