12ccb752aa3b809ceb3eb8f6ed9802ed3d233113
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-03-30 14:10:39 +0200

    build_selfie: Build separate ELFs for QEMU and Kendryte

--- wrapper/k210/build_selfie.sh
+++ wrapper/k210/build_selfie.sh
@@ -10,9 +10,12 @@ riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffrees
 riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o sbi_wrapper.o selfie.o -L../opensbi/build/lib -lsbi -o test.o
 
 # LINKER SCRIPT PREPROCESSING
-riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x1A000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.preprocessed.ld
+riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x1A000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.kendryte.ld
+riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x200000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.qemu.ld
 
 # LINKING
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.preprocessed.ld
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.kendryte.ld
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o qemu-payload.elf -T ./test.elf.qemu.ld
 
 riscv64-unknown-elf-objcopy -S -O binary own-payload.elf own-payload.bin
+riscv64-unknown-elf-objcopy -S -O binary qemu-payload.elf qemu-payload.bin
