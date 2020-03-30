89213b5e59fa9236caaafd94aadea1fc680eef20
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-03-30 15:12:26 +0200

    sbi_wrapper: Hard-code hello world example

--- wrapper/sbi_wrapper.c
+++ wrapper/sbi_wrapper.c
@@ -2,7 +2,18 @@
 
 #include <stdint.h>
 
+#define NUM_FILES 1
+
 int main(int argc, char** argv);
+
+static char* files[NUM_FILES] = {
+    "uint64_t*foo;uint64_t*main(){foo=\"Hello World!    \";while(*foo!=0){write(1,foo,8);foo=foo+1;}}",
+};
+
+static uint64_t file_pos[NUM_FILES] = {
+    0
+};
+
 
 void sbi_ecall_console_putc(char c) {
     asm volatile(
@@ -17,8 +28,23 @@ void sbi_ecall_console_putc(char c) {
 }
 
 
-ssize_t read(int fd, void* buf, size_t count) {
-    return 0;
+ssize_t read(int fd, char* buf, size_t count) {
+    if (fd >= NUM_FILES+1) {
+        return -1;
+    } else {
+        uint64_t num_read = 0;
+        while (count) {
+            uint64_t pos = file_pos[fd-1]++;
+            if (files[fd-1][pos] == '\0')
+                break;
+
+            *(buf++) = files[fd-1][pos];
+
+            --count;
+            num_read++;
+        }
+        return num_read;
+    }
 }
 
 ssize_t write(int fd, const char* buf, size_t count) {
@@ -37,7 +63,7 @@ void exit(int status) {
 }
 
 int open(const char* pathname, int flags) {
-    return -1;
+    return 1;
 }
 
 
@@ -65,6 +91,10 @@ void bootstrap() {
 
     char* args[] = {
         "./selfie",
+        "-c",
+        "hello-world.c",
+        "-m",
+        "1",
         (char*)0,
     };
     int i = 0;
@@ -81,7 +111,7 @@ void bootstrap() {
     write(1, "    <END>\n", 10);
     sbi_ecall_console_putc('\n');
 
-    int exit = main(1, args);
+    int exit = main(5, args);
 
 
     write(1, "\n\nFunction main terminated with exit code ", 42);
