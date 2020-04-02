96812bc188d409c16b59fa7d381996c2bddd5aa1
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-04-02 15:13:05 +0200

    build_opensbi: Add HiFive Unleashed

--- wrapper/k210/build_opensbi.sh
+++ wrapper/k210/build_opensbi.sh
@@ -15,4 +15,5 @@ cd ../opensbi
 
 PLATFORM_RISCV_XLEN=64 make PLATFORM=qemu/virt O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/qemu-payload.bin
 PLATFORM_RISCV_XLEN=64 make PLATFORM=kendryte/k210 O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/own-payload.bin FW_PAYLOAD_OFFSET=0x1A000
+PLATFORM_RISCV_XLEN=64 make PLATFORM=sifive/fu540 O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/qemu-payload.bin FW_PAYLOAD_OFFSET=0x200000
 riscv64-unknown-elf-objcopy -S -O binary --file-alignment 4096 --gap-fill 0xFF build/platform/kendryte/k210/firmware/fw_payload.elf ../k210/test.bin
