77fc2bafb4f6c87aa19b097731050602f6d88464
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-04-09 12:30:01 +0200

    build_selfie.sh: Add -mno-relax

--- wrapper/k210/build_selfie.sh
+++ wrapper/k210/build_selfie.sh
@@ -2,9 +2,9 @@
 
 # COMPILATION
 #riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib -c sbi_wrapper.c -I../opensbi/include -o libsbi_wrapper.o
-riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../../selfie.c -o selfie.o -D'uint64_t = unsigned long long int'
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
-riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../sbi_wrapper.c -o sbi_wrapper.o -I../opensbi/include -L../opensbi/build/lib/ -lsbi
+riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -mno-relax -nostdlib -c ../../selfie.c -o selfie.o -D'uint64_t = unsigned long long int'
+riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -mno-relax -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
+riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -mno-relax -nostdlib -c ../sbi_wrapper.c -o sbi_wrapper.o -I../opensbi/include -L../opensbi/build/lib/ -lsbi
 
 # MERGE OBJECT FILES
 riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o sbi_wrapper.o selfie.o -L../opensbi/build/lib -lsbi -o test.o
