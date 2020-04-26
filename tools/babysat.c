d0d6f3081e421982677409044b1f0e7d43455b58
Christoph Kirsch <ckirsch@gmail.com>
2020-04-26 11:26:46 +0200

    Improving babysat comments [skip ci]

--- tools/babysat.c
+++ tools/babysat.c
@@ -10,12 +10,15 @@ in Austria. For further information and code please refer to:
 http://selfie.cs.uni-salzburg.at
 
 Babysat is a naive implementation of a SAT solver for educational
-purposes. Babysat enumerates all possible variable assignments and
-checks satisfiability for each without any optimizations whatsoever.
-Babysat can therefore also be seen as an executable specification of
-a SAT solver. Babysat comes with a DIMACS CNF parser, is written in C*,
-and uses code from the selfie system. See the Makefile for details on
-how to build babysat.
+purposes. It is arguably even more naive than Donald Knuth's sat0.
+Babysat enumerates all possible variable assignments and checks
+satisfiability for each without any optimizations whatsoever.
+The implementation can therefore also be seen as an executable
+specification of a SAT solver.
+
+Babysat comes with a DIMACS CNF parser, is written in C*, and
+uses code from the selfie system. See selfie's Makefile for
+details on how to build babysat.
 */
 
 // -----------------------------------------------------------------
