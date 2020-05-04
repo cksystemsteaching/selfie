2ad1b40b7e36f9da7a97d2feea09000a5ca1aac9
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-05-04 12:29:23 +0200

    Move from riscv64-unknown-elf to riscv64-elf

--- wrapper/k210/build_opensbi.sh
+++ wrapper/k210/build_opensbi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export CROSS_COMPILE=riscv64-unknown-elf-
+export CROSS_COMPILE=riscv64-elf-
 
 if [ ! -d "../opensbi" ]; then
     git clone git@github.com:riscv/opensbi.git
@@ -16,4 +16,4 @@ cd ../opensbi
 PLATFORM_RISCV_XLEN=64 make PLATFORM=qemu/virt O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/qemu-payload.bin
 PLATFORM_RISCV_XLEN=64 make PLATFORM=kendryte/k210 O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/own-payload.bin FW_PAYLOAD_OFFSET=0x1A000
 PLATFORM_RISCV_XLEN=64 make PLATFORM=sifive/fu540 O=build/ FW_PAYLOAD=y FW_PAYLOAD_PATH=../k210/qemu-payload.bin FW_PAYLOAD_OFFSET=0x200000
-riscv64-unknown-elf-objcopy -S -O binary --file-alignment 4096 --gap-fill 0xFF build/platform/kendryte/k210/firmware/fw_payload.elf ../k210/test.bin
+${CROSS_COMPILE}objcopy -S -O binary --file-alignment 4096 --gap-fill 0xFF build/platform/kendryte/k210/firmware/fw_payload.elf ../k210/test.bin
--- wrapper/k210/build_selfie.sh
+++ wrapper/k210/build_selfie.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/bash
 
+if [ "x$PREFIX" == "x" ]
+then
+    PREFIX="riscv64-elf-"
+fi
+
+if [ "x$CC" == "x" ]
+then
+    CC=${PREFIX}gcc
+fi
+if [ "x$LD" == "x" ]
+then
+    LD=${PREFIX}ld
+fi
+
 # COMPILATION
 #riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -ffreestanding -nostdlib -c sbi_wrapper.c -I../opensbi/include -o libsbi_wrapper.o
-riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../../selfie.c -o selfie.o -D'uint64_t = unsigned long long int'
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
-riscv64-unknown-elf-gcc -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../sbi_wrapper.c -o sbi_wrapper.o -I../opensbi/include -L../opensbi/build/lib/ -lsbi
+${CC} -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../../selfie.c -o selfie.o -D'uint64_t = unsigned long long int'
+${CC} -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -I../opensbi/include  -c hello-world.S -o hello_world_S.o
+${CC} -g3 -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib -c ../sbi_wrapper.c -o sbi_wrapper.o -I../opensbi/include -L../opensbi/build/lib/ -lsbi
 
 # MERGE OBJECT FILES
-riscv64-unknown-elf-ld -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o sbi_wrapper.o selfie.o -L../opensbi/build/lib -lsbi -o test.o
+${LD} -r -b elf64-littleriscv -m elf64lriscv hello_world_S.o sbi_wrapper.o selfie.o -L../opensbi/build/lib -lsbi -o test.o
 
 # LINKER SCRIPT PREPROCESSING
-riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x1A000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.kendryte.ld
-riscv64-unknown-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x200000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.qemu.ld
+riscv64-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x1A000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.kendryte.ld
+riscv64-elf-cpp  -DFW_TEXT_START=0x80000000 -DFW_PAYLOAD_OFFSET=0x200000 -DFW_PAYLOAD_ALIGN=0x1000 -x c test.elf.ldS | grep -v '#' > test.elf.qemu.ld
 
 # LINKING
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.kendryte.ld
-riscv64-unknown-elf-gcc -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o qemu-payload.elf -T ./test.elf.qemu.ld
+${CC} -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o own-payload.elf -T ./test.elf.kendryte.ld
+${CC} -mabi=lp64 -march=rv64imafdc -mcmodel=medany -ffreestanding -nostdlib test.o -L../opensbi/build/lib -o qemu-payload.elf -T ./test.elf.qemu.ld
 
-riscv64-unknown-elf-objcopy -S -O binary own-payload.elf own-payload.bin
-riscv64-unknown-elf-objcopy -S -O binary qemu-payload.elf qemu-payload.bin
+${PREFIX}objcopy -S -O binary own-payload.elf own-payload.bin
+${PREFIX}objcopy -S -O binary qemu-payload.elf qemu-payload.bin
