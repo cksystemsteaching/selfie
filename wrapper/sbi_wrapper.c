de9bf5a344a157886259b5f8d7493ad4dc9f5a49
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-05-06 14:03:31 +0200

    Implement memmove, memcmp and memset
    These functions are required by libgcc

--- wrapper/sbi_wrapper.c
+++ wrapper/sbi_wrapper.c
@@ -8,8 +8,11 @@
 
 int main(int argc, char** argv);
 
-void* memcpy (void* destination, const void* source, size_t num) {
-    uint8_t* dst = (uint8_t*) destination;
+
+// Function required by libgcc for a freestanding environment
+
+void* memmove(void* dest, const void* source, size_t num) {
+    uint8_t* dst = (uint8_t*) dest;
     uint8_t* src = (uint8_t*) source;
 
     while (num > 0) {
@@ -17,7 +20,35 @@ void* memcpy (void* destination, const void* source, size_t num) {
         num--;
     }
 
-    return destination;
+    return dest;
+}
+void* memcpy (void* destination, const void* source, size_t num) {
+    return memmove(destination, source, num);
+}
+void* memset(void* ptr, int value, size_t num) {
+    uint8_t* charPtr = (uint8_t*) ptr;
+
+    while (num > 0) {
+        *charPtr = (uint8_t) value;
+
+        charPtr++;
+        num--;
+    }
+
+    return ptr;
+}
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+    uint8_t* p1 = (uint8_t*) ptr1;
+    uint8_t* p2 = (uint8_t*) ptr2;
+
+    for (size_t i = 0; i < num; i++) {
+        if (p1[i] < p2[i])
+            return -1;
+        if (p1[i] > p2[i])
+            return 1;
+    }
+
+    return 0;
 }
 
 
