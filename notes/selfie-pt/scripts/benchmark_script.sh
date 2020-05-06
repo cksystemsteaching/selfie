8077d19023e3078a4f9a11866f98137b471b3898
Martin Fischer <martin.fischer@stud.sbg.ac.at>
2020-05-06 10:30:23 +0200

    add cpu model information to benchmark script

--- notes/selfie-pt/scripts/benchmark_script.sh
+++ notes/selfie-pt/scripts/benchmark_script.sh
@@ -14,12 +14,14 @@ pushd ../../../
 for i in `seq 1 3`; do
     make clean
     echo -e "\n\ncurrent time is $( date -Iseconds )\n\n"
-    /usr/bin/time -v -o "notes/selfie-pt/scripts/benchmark-output/tree_$( uname -r)_$( date -Iseconds ).txt" make all
+    # cpu model extraction from https://askubuntu.com/questions/988440/how-do-i-get-the-model-name-of-my-processor/988449#988449
+    /usr/bin/time -v -o "notes/selfie-pt/scripts/benchmark-output/tree_$( lscpu | sed -nr "/Model name/ s/.*:\s*(.*) @ .*/\1/p" | sed "s/ /_/g" )_$( uname -r)_$( date -Iseconds ).txt" make all
     sed -i "s/PAGE_TABLE_TREE \+= 1/PAGE_TABLE_TREE = 0/g" selfie.c
 
     echo -e "\n\ncurrent time is $( date -Iseconds )\n\n"
     make clean
-    /usr/bin/time -v -o "notes/selfie-pt/scripts/benchmark-output/linear_$( uname -r)_$( date -Iseconds ).txt" make all
+    # cpu model extraction from https://askubuntu.com/questions/988440/how-do-i-get-the-model-name-of-my-processor/988449#988449
+    /usr/bin/time -v -o "notes/selfie-pt/scripts/benchmark-output/linear_$( lscpu | sed -nr "/Model name/ s/.*:\s*(.*) @ .*/\1/p" | sed "s/ /_/g" )_$( uname -r)_$( date -Iseconds ).txt" make all
     sed -i "s/PAGE_TABLE_TREE \+= 0/PAGE_TABLE_TREE = 1/g" selfie.c
 done
 
