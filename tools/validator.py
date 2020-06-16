91ab260c74f5cffe8702070cf870920b0b798e60
Gotsiller <chri.si@gmx.at>
2020-06-16 14:47:35 +0200

    changes btormc timeout to 5min

--- tools/validator.py
+++ tools/validator.py
@@ -307,7 +307,7 @@ arguments.add_argument("-b", "--btormc", dest="btormc_path", default="btormc",
                        help="path to btormc executable")
 arguments.add_argument("-ts", "--timeout_selfie", dest="selfie_timeout", default="10s",
                        help="timeout for execution of in_file on mipster (example: 10s, 5m, 1h)")
-arguments.add_argument("-tb", "--timeout_btormc", dest="btormc_timeout", default="9m",
+arguments.add_argument("-tb", "--timeout_btormc", dest="btormc_timeout", default="5m",
                        help="timeout for execution of btormc with the generated btor2 file (example: 10s, 5m, 1h)")
 arguments.add_argument("-kmax", dest="kmax", type=int, default=10000, help="-kmax parameter for btormc")
 arguments.add_argument("-mem", "--memory", dest="memory", type=int, default="2", help="memory [MB] for mipster")
