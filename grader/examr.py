a13c2533455dfa03781763d0339624ec7441275c
Christoph Kirsch <ckirsch@gmail.com>
2020-07-02 12:21:32 +0200

    Adding Python script for processing online exams [skip ci]

---
+++ grader/examr.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import sys, getopt
+
+import csv
+
+from collections import namedtuple
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('examr.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('examr.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    if inputfile != '':
+        with open(inputfile, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            if outputfile != '':
+                with open(outputfile, mode='w') as csv_file:
+                    students = dict()
+
+                    qa_count       = 0
+                    q_length_count = 0
+                    a_length_count = 0
+                    for row in csv_reader:
+                        qa_count     += 1
+                        q_length_count += len(row['Ask Question'])
+                        a_length_count += len(row['Answer Question'])
+
+                        entry = namedtuple('Entry', ['number_of_qas', 'q_average', 'q_length', 'a_average', 'a_length'])
+
+                        if row['Email address'] not in students:
+                            students[row['Email address']] = entry(
+                                    number_of_qas = 1,
+                                    q_average     = float(row['Grade Question']),
+                                    q_length      = len(row['Ask Question']),
+                                    a_average     = float(row['Grade Answer']),
+                                    a_length      = len(row['Answer Question'])
+                                )
+                        else:
+                            students[row['Email address']] = entry(
+                                    number_of_qas = students[row['Email address']].number_of_qas + 1,
+                                    q_average     = (students[row['Email address']].q_average + float(row['Grade Question'])) / 2,
+                                    q_length      = students[row['Email address']].q_length + len(row['Ask Question']),
+                                    a_average     = (students[row['Email address']].a_average + float(row['Grade Answer'])) / 2,
+                                    a_length      = students[row['Email address']].a_length + len(row['Answer Question']),
+                                )
+                    print(f'Total number of Q&As {qa_count}')
+
+                    fieldnames = 'Google Apps Email', 'PLUS Email', 'Total Average', 'Number of Q&As', 'Length of Answers', 'Length of Questions', 'Totel Length of Q&As', 'Question Average', 'Answer Average'
+                    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+                    csv_writer.writeheader()
+                    student_count = 0
+                    for student in list(students):
+                        student_count += 1
+                        csv_writer.writerow({
+                                'Google Apps Email': student,
+                                'Total Average': (students[student].q_average + students[student].a_average) / 2,
+                                'Number of Q&As': students[student].number_of_qas,
+                                'Length of Answers': students[student].a_length,
+                                'Length of Questions': students[student].q_length,
+                                'Totel Length of Q&As': students[student].q_length + students[student].a_length,
+                                'Question Average': students[student].q_average,
+                                'Answer Average': students[student].a_average
+                            })
+
+                    print(f'Number of students: {student_count}')
+                    print(f'Average number of Q&As per student: {qa_count / student_count}')
+                    print(f'Average length of answers per student: {a_length_count / student_count}')
+                    print(f'Average length of questions per student: {q_length_count / student_count}')
+                    print(f'Average length of Q&As per student: {(q_length_count + a_length_count) / student_count}')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
