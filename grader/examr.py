f79c0c07c02d828335c47d1d85665263330c6d2d
Christoph Kirsch <ckirsch@gmail.com>
2020-07-04 10:22:08 +0200

    Better python for examr [skip ci]

--- grader/examr.py
+++ grader/examr.py
@@ -4,7 +4,94 @@ import sys, getopt
 
 import csv
 
-from collections import namedtuple
+# import textdistance
+
+class Student:
+    def __init__(self, q_total, q_length, a_total, a_length):
+        self.number_of_qas = 1
+        self.q_total       = q_total
+        self.q_length      = q_length
+        self.q_similarity  = 0
+        self.a_total       = a_total
+        self.a_length      = a_length
+        self.a_similarity  = 0
+
+def process_qas(csv_file):
+    csv_reader = csv.DictReader(csv_file)
+
+    students = dict()
+
+    emails = []
+
+    questions = []
+    answers   = []
+
+    q_total_length = 0
+    a_total_length = 0
+
+    for row in csv_reader:
+        emails.append(row['Email address'])
+
+        questions.append(row['Ask Question'])
+        q_total_length += len(row['Ask Question'])
+        answers.append(row['Answer Question'])
+        a_total_length += len(row['Answer Question'])
+
+        if row['Email address'] not in students:
+            students[row['Email address']] = Student(
+                float(row['Grade Question']),
+                len(row['Ask Question']),
+                float(row['Grade Answer']),
+                len(row['Answer Question']))
+        else:
+            students[row['Email address']].number_of_qas += 1
+            students[row['Email address']].q_total       += float(row['Grade Question'])
+            students[row['Email address']].q_length      += len(row['Ask Question'])
+            students[row['Email address']].a_total       += float(row['Grade Answer'])
+            students[row['Email address']].a_length      += len(row['Answer Question'])
+
+    return students, emails, questions, answers, q_total_length, a_total_length
+
+def generate_csv(students, csv_file):
+    fieldnames = 'Google Apps Email', 'PLUS Email', 'Total Average', 'Number of Q&As', 'Length of Answers', 'Similarity of Answers', 'Length of Questions', 'Similarity of Questions', 'Totel Length of Q&As', 'Question Average', 'Answer Average'
+
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    csv_writer.writeheader()
+
+    for student in students.items():
+        csv_writer.writerow({
+            'Google Apps Email': student[0],
+            'Total Average': (student[1].q_total + student[1].a_total) / student[1].number_of_qas / 2,
+            'Number of Q&As': student[1].number_of_qas,
+            'Length of Answers': student[1].a_length,
+            'Similarity of Answers': student[1].a_similarity,
+            'Length of Questions': student[1].q_length,
+            'Similarity of Questions': student[1].q_similarity,
+            'Totel Length of Q&As': student[1].q_length + student[1].a_length,
+            'Question Average': student[1].q_total / student[1].number_of_qas,
+            'Answer Average': student[1].a_total / student[1].number_of_qas
+        })
+
+def compute_similarity(text_blocks):
+    similarity = [ [0] * len(text_blocks) for i in range(len(text_blocks)) ]
+
+    for x in range(len(text_blocks)):
+        for y in range(len(text_blocks)):
+            if text_blocks[x] == text_blocks[y]:
+                similarity[x][y] = 1
+            else:
+                similarity[x][y] = 0
+    return similarity
+
+def assign_similarity(students, emails, q_similarity, a_similarity):
+    for x in range(len(emails)):
+        student = students[emails[x]]
+
+        for y in range(len(emails)):
+            if x != y:
+                student.q_similarity += q_similarity[x][y]
+                student.a_similarity += a_similarity[x][y]
 
 def main(argv):
     inputfile = ''
@@ -23,63 +110,24 @@ def main(argv):
         elif opt in ("-o", "--ofile"):
             outputfile = arg
     if inputfile != '':
-        with open(inputfile, mode='r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
+        with open(inputfile, mode='r') as csv_input_file:
             if outputfile != '':
-                with open(outputfile, mode='w') as csv_file:
-                    students = dict()
+                with open(outputfile, mode='w') as csv_output_file:
+                    students, emails, questions, answers, q_total_length, a_total_length = process_qas(csv_input_file)
 
-                    qa_count       = 0
-                    q_length_count = 0
-                    a_length_count = 0
-                    for row in csv_reader:
-                        qa_count       += 1
-                        q_length_count += len(row['Ask Question'])
-                        a_length_count += len(row['Answer Question'])
+                    q_similarity = compute_similarity(questions)
+                    a_similarity = compute_similarity(answers)
 
-                        entry = namedtuple('Entry', ['number_of_qas', 'q_total', 'q_length', 'a_total', 'a_length'])
+                    assign_similarity(students, emails, q_similarity, a_similarity)
 
-                        if row['Email address'] not in students:
-                            students[row['Email address']] = entry(
-                                    number_of_qas = 1,
-                                    q_total       = float(row['Grade Question']),
-                                    q_length      = len(row['Ask Question']),
-                                    a_total       = float(row['Grade Answer']),
-                                    a_length      = len(row['Answer Question'])
-                                )
-                        else:
-                            students[row['Email address']] = entry(
-                                    number_of_qas = students[row['Email address']].number_of_qas + 1,
-                                    q_total       = students[row['Email address']].q_total + float(row['Grade Question']),
-                                    q_length      = students[row['Email address']].q_length + len(row['Ask Question']),
-                                    a_total       = students[row['Email address']].a_total + float(row['Grade Answer']),
-                                    a_length      = students[row['Email address']].a_length + len(row['Answer Question']),
-                                )
-                    print(f'Total number of Q&As {qa_count}')
+                    generate_csv(students, csv_output_file)
 
-                    fieldnames = 'Google Apps Email', 'PLUS Email', 'Total Average', 'Number of Q&As', 'Length of Answers', 'Length of Questions', 'Totel Length of Q&As', 'Question Average', 'Answer Average'
-                    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-                    csv_writer.writeheader()
-                    student_count = 0
-                    for student in list(students):
-                        student_count += 1
-                        csv_writer.writerow({
-                                'Google Apps Email': student,
-                                'Total Average': (students[student].q_total + students[student].a_total) / students[student].number_of_qas / 2,
-                                'Number of Q&As': students[student].number_of_qas,
-                                'Length of Answers': students[student].a_length,
-                                'Length of Questions': students[student].q_length,
-                                'Totel Length of Q&As': students[student].q_length + students[student].a_length,
-                                'Question Average': students[student].q_total / students[student].number_of_qas,
-                                'Answer Average': students[student].a_total / students[student].number_of_qas
-                            })
-
-                    print(f'Number of students: {student_count}')
-                    print(f'Average number of Q&As per student: {qa_count / student_count}')
-                    print(f'Average length of answers per student: {a_length_count / student_count}')
-                    print(f'Average length of questions per student: {q_length_count / student_count}')
-                    print(f'Average length of Q&As per student: {(q_length_count + a_length_count) / student_count}')
+                    print(f'Number of students: {len(students)}')
+                    print(f'Total number of Q&As {len(questions)}')
+                    print(f'Average number of Q&As per student: {len(questions) / len(students)}')
+                    print(f'Average length of answers per student: {a_total_length / len(students)}')
+                    print(f'Average length of questions per student: {q_total_length / len(students)}')
+                    print(f'Average length of Q&As per student: {(q_total_length + a_total_length) / len(students)}')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
