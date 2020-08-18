585c763292beebbb9de5d78e921236671bb7fde0
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-08-18 13:10:38 +0200

    glue_libraryos.c: Fix compile warnings

--- machine/glue_libraryos.c
+++ machine/glue_libraryos.c
@@ -3,28 +3,30 @@
 #include "syscall.h"
 #include <stdint.h>
 
+#include "compiler-utils.h"
 
 static FILEDESC open_files[NUM_FDS];
 void* heap_head;
 
 ssize_t read(int fd, char* buf, size_t count) {
-    return kread(fd, buf, count, open_files, NUM_FDS);
+  return kread(fd, buf, count, open_files, NUM_FDS);
 }
 
 intmax_t write(int fd, const char* buf, size_t count) {
-    return kwrite(fd, buf, count, open_files, NUM_FDS);
+  return kwrite(fd, buf, count, open_files, NUM_FDS);
 }
 
 uint64_t open(char* filename, uint64_t flags, uint64_t mode) {
-     return kopen(filename, flags, open_files, NUM_FDS);
+  UNUSED_VAR(mode);
+  return kopen(filename, flags, open_files, NUM_FDS);
 }
 
 void* malloc(unsigned long long size) {
-    return kmalloc(size, &heap_head);    
+  return kmalloc(size, &heap_head);    
 }
 
 extern void _start_hang();
 void exit(int status) {
-    printf(">EXIT called with exit code %x<\n", status);
-    _start_hang();
+  printf(">EXIT called with exit code %x<\n", status);
+  _start_hang();
 }
