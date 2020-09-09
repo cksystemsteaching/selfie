761e543738354283cf03ec7bc4897ea00791fc66
Martin Fischer <martin.fischer@stud.sbg.ac.at>
2020-09-09 17:49:33 +0200

    mark 64-bit literal as 'ULL'

--- machine/mmu.c
+++ machine/mmu.c
@@ -138,7 +138,7 @@ uint64_t vaddr_to_vpn(uint64_t vaddr) {
 uint64_t vpn_to_vaddr(uint64_t vpn) {
   // Sv39 requires virtual addresses to sign-extend bit 38
   // The bitmask to OR to this is (2^64 - 1) - (2^39 - 1) = 0xFFFFFF8000000000
-  const uint64_t SIGN_EXTEND_BIT38 = 0xFFFFFF8000000000;
+  const uint64_t SIGN_EXTEND_BIT38 = 0xFFFFFF8000000000ULL;
 
   uint64_t vaddr = vpn << 12;
 
