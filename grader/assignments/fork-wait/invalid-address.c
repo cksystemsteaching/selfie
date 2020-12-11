ed4292308a7eb8f8c61c2638d327fb68d27547f7
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-12-11 21:49:14 +0100

    Grader: Extend invalid address test of `fork-wait-exit`
    This commit extends the invalid-address.c test to check whether the
    zombie has not been consumed erroneously after providing an invalid
    address

--- grader/assignments/fork-wait/invalid-address.c
+++ grader/assignments/fork-wait/invalid-address.c
@@ -1,16 +1,28 @@
 uint64_t VIRTUALMEMORYSIZE = 4294967296; // 4GB of virtual memory
 
+// computes the exit code from the status value
+// exit code is in the least significant bits from 8 to 16
+uint64_t wexitstatus(uint64_t status) {
+  return status * 281474976710656 / 72057594037927936;
+}
+
 int main() {
-  uint64_t pid;
+  uint64_t pid1;
+  uint64_t pid2;
   uint64_t* code;
 
   code = (uint64_t*) VIRTUALMEMORYSIZE;
 
-  pid = fork();
-  if (pid == 0)
-      exit(0);
+  pid1 = fork();
+  if (pid1 == 0)
+    exit(5);
   else
-      pid = wait(code);
+    pid1 = wait(code);
 
-  return 43 + pid;
+  // Actually query the PID/status
+  // The previous call should have failed before, but mustn't have consumed the zombie
+  code = malloc(8);
+  pid2 = wait(code);
+
+  return 36 + pid1 + pid2 + wexitstatus(*code);
 }
