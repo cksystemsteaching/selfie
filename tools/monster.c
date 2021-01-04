9b97426301c22228a903057b670c9f70bb885c6c
Christoph Kirsch <ckirsch@gmail.com>
2021-01-04 16:08:32 +0100

    Happy New Year 2021! [skip ci]

--- tools/monster.c
+++ tools/monster.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015-2020, the Selfie Project authors. All rights reserved.
+Copyright (c) 2015-2021, the Selfie Project authors. All rights reserved.
 Please see the AUTHORS file for details. Use of this source code is
 governed by a BSD license that can be found in the LICENSE file.
 
