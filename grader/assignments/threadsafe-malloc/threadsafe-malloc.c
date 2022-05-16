87e7e956fd237d049bee6987494c1a41a3b6d7ea
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-05-16 13:35:29 +0200

    Improve thread-safe.c test file

--- grader/assignments/threadsafe-malloc/threadsafe-malloc.c
+++ grader/assignments/threadsafe-malloc/threadsafe-malloc.c
@@ -3,14 +3,6 @@ uint64_t write(uint64_t fd, uint64_t* buffer, uint64_t bytes_to_write);
 uint64_t pthread_create();
 uint64_t pthread_join(uint64_t* wstatus);
 
-uint64_t* c;
-
-void print_integer(uint64_t i) {
-  // single character number
-  *c = 32 * 256 + 48 + i;
-  write(1, c, 2);
-}
-
 int main(int argc, char** argv) {
   uint64_t pid;
   uint64_t* sum;
@@ -18,28 +10,32 @@ int main(int argc, char** argv) {
 
   sum2 = malloc(8);
 
-  c = malloc(8);
-
   pid = pthread_create();
 
   sum = malloc(8 * 2);
 
-
   if (pid) {
+    // main thread
+
     pthread_join(sum2);
 
-    *sum = 11;
-    *(sum + 1) = 12;
-
+    // sum2 points to sum which points to 18 and 19
+    // so dereference to make sum2 point directly to 18 and 19
     sum2 = (uint64_t*) *sum2;
 
+    *sum = 2;
+    *(sum + 1) = 3;
+
+    // success: 2 + 3 + 18 + 19 = 42
+    // failure: 2 + 3 + 2 + 3 = 10  (18 and 19 were overwritten)
     return *sum + *(sum + 1) + *sum2 + *(sum2 + 1);
+
   } else {
-    *sum = 9;
-    *(sum + 1) = 10;
+    // child thread
+
+    *sum = 18;
+    *(sum + 1) = 19;
 
     return (uint64_t) sum;
   }
-
-  //return 0;
 }
