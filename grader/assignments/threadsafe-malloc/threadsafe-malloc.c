09f4a430c46a031da7a07bf16159389271b17053
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-05-18 11:57:55 +0200

    Update threadsafe-malloc.c

--- grader/assignments/threadsafe-malloc/threadsafe-malloc.c
+++ grader/assignments/threadsafe-malloc/threadsafe-malloc.c
@@ -1,39 +1,109 @@
 uint64_t pthread_create();
 uint64_t pthread_join(uint64_t* wstatus);
+void pthread_exit(uint64_t code);
+
+uint64_t thread;
+
+void force_switch() {
+  uint64_t pid;
+
+  // forces a switch
+  pid = pthread_create();
+
+  // newly created thread immediately runs out
+  if (pid == 0)
+    pthread_exit(0);
+  else
+    pthread_join((uint64_t*) 0);
+}
 
 int main(int argc, char** argv) {
   uint64_t pid;
-  uint64_t* sum;
-  uint64_t* sum2;
+  uint64_t counter;
+  uint64_t i;
+  uint64_t* p1;
+  uint64_t* p2;
 
-  sum2 = malloc(8);
+  thread = -1;
 
   pid = pthread_create();
 
-  sum = malloc(8 * 2);
+  if (thread == -1)
+    thread = pid;
+
+  // make sure the child thread is running first
+  if (thread != 0)
+    force_switch();
+
+  // assert: thread == 1
+
+  counter = 1111108;
+
+  i = 4;
 
   if (pid) {
-    // main thread
+    // parent
 
-    pthread_join(sum2);
+    // --------------- 2nd ---------------
 
-    // sum2 points to sum which points to 18 and 19
-    // so dereference to make sum2 point directly to 18 and 19
-    sum2 = (uint64_t*) *sum2;
+    // reset the timer
+    force_switch();
 
-    *sum = 2;
-    *(sum + 1) = 3;
+    // --------------- 4th ---------------
 
-    // success: 2 + 3 + 18 + 19 = 42
-    // failure: 2 + 3 + 2 + 3 = 10  (18 and 19 were overwritten)
-    return *sum + *(sum + 1) + *sum2 + *(sum2 + 1);
+    p1 = malloc(16);
+    p2 = malloc(16);
 
+    pthread_join(p2); // switch
+
+    // --------------- 6th ---------------
+
+    // p2 points to other p2 which points to 2 and 3
+
+    p2 = (uint64_t*) *p2;
+
+    // now p2 directly points to 2 and 3
+
+    *p1 = 2;
+    *(p1 + 1) = 3;
+
+    return *p1 + *(p1 + 1) + *p2 + *(p2 + 1);
   } else {
-    // child thread
+    // child
 
-    *sum = 18;
-    *(sum + 1) = 19;
+    // --------------- 1st ---------------
 
-    return (uint64_t) sum;
+    // reset the timer
+    force_switch();
+
+    // --------------- 3rd ---------------
+
+    // 9 instructions from force_switch()
+
+    // 2 instructions
+    i = 0;
+
+    // 9 instructions per loop
+    // 1111108 * 9 = 9999972
+    while (i < counter)
+      i = i + 1;
+
+    // 4 instructions on loop end
+
+    // so far 9 + 2 + 9999972 + 4 = 9999987
+
+    // 13 instructions left before switch by timeout
+
+    // lr is 12th instruction of malloc() call
+
+    // switch should happen in here after lr but before sc
+    p2 = malloc(16);
+
+    // --------------- 5th ---------------
+
+    *p2 = 18;
+    *(p2 + 1) = 19;
+
+    return (uint64_t) p2;
   }
 }
