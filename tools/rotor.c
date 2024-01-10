0457363aa452ec3a6de33ab1e534174e1db77d8a
Christoph Kirsch <ckirsch@gmail.com>
2024-01-10 21:23:18 +0100

    Full RV32M support

--- tools/rotor.c
+++ tools/rotor.c
@@ -150,7 +150,9 @@ char* OP_SRA = (char*) 0;
 char* OP_ADD  = (char*) 0;
 char* OP_SUB  = (char*) 0;
 char* OP_MUL  = (char*) 0;
+char* OP_SDIV = (char*) 0;
 char* OP_UDIV = (char*) 0;
+char* OP_SREM = (char*) 0;
 char* OP_UREM = (char*) 0;
 
 char* OP_CONCAT = (char*) 0;
@@ -219,7 +221,9 @@ void init_model() {
   OP_ADD  = "add";
   OP_SUB  = "sub";
   OP_MUL  = "mul";
+  OP_SDIV = "sdiv";
   OP_UDIV = "udiv";
+  OP_SREM = "srem";
   OP_UREM = "urem";
 
   OP_CONCAT = "concat";
@@ -317,6 +321,8 @@ uint64_t* SID_INSTRUCTION_WORD           = (uint64_t*) 0;
 uint64_t* NID_INSTRUCTION_WORD_SIZE_MASK = (uint64_t*) 0;
 
 uint64_t* NID_MAX_STRING_LENGTH = (uint64_t*) 0;
+
+uint64_t* SID_DOUBLE_MACHINE_WORD = (uint64_t*) 0;
 
 // ------------------------- INITIALIZATION ------------------------
 
@@ -420,6 +426,8 @@ void init_interface_sorts() {
   NID_INSTRUCTION_WORD_SIZE_MASK = NID_MACHINE_WORD_3;
 
   NID_MAX_STRING_LENGTH = new_constant(OP_CONSTD, SID_MACHINE_WORD, MAX_STRING_LENGTH, 0, "maximum string length");
+
+  SID_DOUBLE_MACHINE_WORD = new_bitvec(2 * WORDSIZEINBITS, "double machine word");
 }
 
 // -----------------------------------------------------------------
@@ -844,9 +852,12 @@ uint64_t* decode_op(uint64_t* sid, uint64_t* ir_nid,
   uint64_t* add_nid, uint64_t* sub_nid,
   uint64_t* slt_nid, uint64_t* sltu_nid,
   uint64_t* xor_nid, uint64_t* or_nid, uint64_t* and_nid,
-  uint64_t* sll_nid, uint64_t* srl_nid, uint64_t* sra_nid,
-  uint64_t* mul_nid, uint64_t* divu_nid, uint64_t* remu_nid, char* comment,
-  uint64_t* no_funct3_nid, uint64_t* no_funct7_nid, uint64_t* other_opcode_nid);
+  uint64_t* sll_nid, uint64_t* srl_nid, uint64_t* sra_nid, char* comment,
+  uint64_t* no_funct3_nid, uint64_t* RV32M_nid, uint64_t* other_opcode_nid);
+uint64_t* decode_RV32M(uint64_t* sid, uint64_t* ir_nid,
+  uint64_t* mul_nid, uint64_t* mulh_nid, uint64_t* mulhsu_nid, uint64_t* mulhu_nid,
+  uint64_t* div_nid, uint64_t* divu_nid, uint64_t* rem_nid, uint64_t* remu_nid, char* comment,
+  uint64_t* no_funct3_nid, uint64_t* no_funct7_nid);
 uint64_t* decode_load(uint64_t* sid, uint64_t* ir_nid,
   uint64_t* ld_nid,
   uint64_t* lw_nid, uint64_t* lwu_nid,
@@ -1093,11 +1104,15 @@ uint64_t* NID_AND = (uint64_t*) 0;
 
 // RV64I codes missing in RISC-U
 
+uint64_t OP_IMM_32 = 27; // 0011011, I format
+uint64_t OP_OP_32  = 59; // 0111011, I format
+
 uint64_t F3_LWU = 6; // 110
 
-uint64_t* NID_F3_LWU = (uint64_t*) 0;
+uint64_t* NID_OP_IMM_32 = (uint64_t*) 0;
+uint64_t* NID_OP_OP_32  = (uint64_t*) 0;
 
-uint64_t OP_IMM_32 = 27; // 0011011, I format
+uint64_t* NID_F3_LWU = (uint64_t*) 0;
 
 // RV64I instruction switches
 
@@ -1130,17 +1145,13 @@ uint64_t* NID_F3_REM    = (uint64_t*) 0;
 
 // RV32M instruction switches
 
+uint64_t RV32M = 1; // RV32M support
+
 uint64_t* NID_MULH   = (uint64_t*) 0;
 uint64_t* NID_MULHSU = (uint64_t*) 0;
 uint64_t* NID_MULHU  = (uint64_t*) 0;
 uint64_t* NID_DIV    = (uint64_t*) 0;
 uint64_t* NID_REM    = (uint64_t*) 0;
-
-// RV64M codes
-
-uint64_t OP_OP_32 = 59; // 0111011, I format
-
-uint64_t* NID_OP_OP_32 = (uint64_t*) 0;
 
 // RV64M instruction switches
 
@@ -1320,16 +1331,19 @@ void init_instruction_sorts() {
   NID_SRLI = NID_TRUE;
   NID_SRAI = NID_TRUE;
 
-  NID_SLL = NID_FALSE;
-  NID_SLT = NID_FALSE;
-  NID_XOR = NID_FALSE;
-  NID_SRL = NID_FALSE;
-  NID_SRA = NID_FALSE;
+  NID_SLL = NID_TRUE;
+  NID_SLT = NID_TRUE;
+  NID_XOR = NID_TRUE;
+  NID_SRL = NID_TRUE;
+  NID_SRA = NID_TRUE;
 
-  NID_OR  = NID_FALSE;
-  NID_AND = NID_FALSE;
+  NID_OR  = NID_TRUE;
+  NID_AND = NID_TRUE;
 
   // RV64I codes missing in RISC-U
+
+  NID_OP_IMM_32 = new_constant(OP_CONST, SID_OPCODE, OP_IMM_32, 7, "OP_IMM_32");
+  NID_OP_OP_32  = new_constant(OP_CONST, SID_OPCODE, OP_OP_32, 7, "OP_OP_32");
 
   NID_F3_LWU = new_constant(OP_CONST, SID_FUNCT3, F3_LWU, 3, "F3_LWU");
 
@@ -1373,15 +1387,21 @@ void init_instruction_sorts() {
 
   // RV32M instruction switches
 
-  NID_MULH   = NID_TRUE;
-  NID_MULHSU = NID_TRUE;
-  NID_MULHU  = NID_TRUE;
-  NID_DIV    = NID_TRUE;
-  NID_REM    = NID_TRUE;
-
-  // RV64M codes
-
-  NID_OP_OP_32 = new_constant(OP_CONST, SID_OPCODE, OP_OP_32, 7, "OP_OP_32");
+  if (RV32M) {
+    NID_MUL    = NID_TRUE;
+    NID_MULH   = NID_TRUE;
+    NID_MULHSU = NID_TRUE;
+    NID_MULHU  = NID_TRUE;
+    NID_DIV    = NID_TRUE;
+    NID_REM    = NID_TRUE;
+  } else {
+    NID_MUL    = NID_FALSE;
+    NID_MULH   = NID_FALSE;
+    NID_MULHSU = NID_FALSE;
+    NID_MULHU  = NID_FALSE;
+    NID_DIV    = NID_FALSE;
+    NID_REM    = NID_FALSE;
+  }
 
   // RV64M instruction switches
 
@@ -3114,9 +3134,8 @@ uint64_t* decode_op(uint64_t* sid, uint64_t* ir_nid,
   uint64_t* add_nid, uint64_t* sub_nid,
   uint64_t* slt_nid, uint64_t* sltu_nid,
   uint64_t* xor_nid, uint64_t* or_nid, uint64_t* and_nid,
-  uint64_t* sll_nid, uint64_t* srl_nid, uint64_t* sra_nid,
-  uint64_t* mul_nid, uint64_t* divu_nid, uint64_t* remu_nid, char* comment,
-  uint64_t* no_funct3_nid, uint64_t* no_funct7_nid, uint64_t* other_opcode_nid) {
+  uint64_t* sll_nid, uint64_t* srl_nid, uint64_t* sra_nid, char* comment,
+  uint64_t* no_funct3_nid, uint64_t* RV32M_nid, uint64_t* other_opcode_nid) {
   return decode_opcode(sid, ir_nid,
     NID_OP_OP, "OP?",
     decode_funct7(sid, ir_nid,
@@ -3157,22 +3176,47 @@ uint64_t* decode_op(uint64_t* sid, uint64_t* ir_nid,
             sra_nid, format_comment("sra %s", (uint64_t) comment),
             no_funct3_nid)),
         format_comment("sub or sra %s", (uint64_t) comment),
-        decode_funct7(sid, ir_nid,
-          NID_F7_MUL_DIV_REM, "MUL or DIVU or REMU?",
-          decode_funct3(sid, ir_nid,
-            NID_F3_ADD_SUB_MUL, "MUL?",
-            mul_nid, format_comment("mul %s", (uint64_t) comment),
-            decode_funct3(sid, ir_nid,
-              NID_F3_DIVU, "DIVU?",
-              divu_nid, format_comment("divu %s", (uint64_t) comment),
-              decode_funct3(sid, ir_nid,
-                NID_F3_REMU, "REMU?",
-                remu_nid, format_comment("remu %s", (uint64_t) comment),
-                no_funct3_nid))),
-          format_comment("mul or divu or remu %s", (uint64_t) comment),
-          no_funct7_nid))),
+        RV32M_nid)),
     format_comment("op %s", (uint64_t) comment),
     other_opcode_nid);
+}
+
+uint64_t* decode_RV32M(uint64_t* sid, uint64_t* ir_nid,
+  uint64_t* mul_nid, uint64_t* mulh_nid, uint64_t* mulhsu_nid, uint64_t* mulhu_nid,
+  uint64_t* div_nid, uint64_t* divu_nid, uint64_t* rem_nid, uint64_t* remu_nid, char* comment,
+  uint64_t* no_funct3_nid, uint64_t* no_funct7_nid) {
+  if (RV32M)
+    return decode_funct7(sid, ir_nid,
+      NID_F7_MUL_DIV_REM, "MUL or DIVU or REMU?",
+      decode_funct3(sid, ir_nid,
+        NID_F3_ADD_SUB_MUL, "MUL?",
+        mul_nid, format_comment("mul %s", (uint64_t) comment),
+        decode_funct3(sid, ir_nid,
+          NID_F3_MULH, "MULH?",
+          mulh_nid, format_comment("mulh %s", (uint64_t) comment),
+          decode_funct3(sid, ir_nid,
+            NID_F3_MULHSU, "MULHSU?",
+            mulhsu_nid, format_comment("mulhsu %s", (uint64_t) comment),
+            decode_funct3(sid, ir_nid,
+              NID_F3_MULHU, "MULHU?",
+              mulhu_nid, format_comment("mulhu %s", (uint64_t) comment),
+              decode_funct3(sid, ir_nid,
+                NID_F3_DIV, "DIV?",
+                div_nid, format_comment("div %s", (uint64_t) comment),
+                decode_funct3(sid, ir_nid,
+                  NID_F3_DIVU, "DIVU?",
+                  divu_nid, format_comment("divu %s", (uint64_t) comment),
+                  decode_funct3(sid, ir_nid,
+                    NID_F3_REM, "REM?",
+                    rem_nid, format_comment("rem %s", (uint64_t) comment),
+                    decode_funct3(sid, ir_nid,
+                      NID_F3_REMU, "REMU?",
+                      remu_nid, format_comment("remu %s", (uint64_t) comment),
+                      no_funct3_nid)))))))),
+      format_comment("mul or mulh or mulhsu or mulhu or div or divu or rem or remu %s", (uint64_t) comment),
+      no_funct7_nid);
+  else
+    return no_funct7_nid;
 }
 
 uint64_t* decode_load(uint64_t* sid, uint64_t* ir_nid,
@@ -3311,10 +3355,17 @@ uint64_t* decode_instruction(uint64_t* ir_nid) {
         NID_SLL,
         NID_SRL,
         NID_SRA,
-        NID_MUL,
-        NID_DIVU,
-        NID_REMU,
-        "known?", NID_FALSE, NID_FALSE,
+        "known?", NID_FALSE,
+        decode_RV32M(SID_BOOLEAN, ir_nid,
+          NID_MUL,
+          NID_MULH,
+          NID_MULHSU,
+          NID_MULHU,
+          NID_DIV,
+          NID_DIVU,
+          NID_REM,
+          NID_REMU,
+          "known?", NID_FALSE, NID_FALSE),
         decode_load(SID_BOOLEAN, ir_nid,
           NID_LD,
           NID_LW, NID_LWU,
@@ -3457,21 +3508,62 @@ uint64_t* op_data_flow(uint64_t* ir_nid, uint64_t* other_data_flow_nid) {
       rs1_value_nid,
       rs2_value_nid,
       "signed rs1 value >> rs2 value"),
-    new_binary(OP_MUL, SID_MACHINE_WORD,
-      rs1_value_nid,
-      rs2_value_nid,
-      "rs1 value * rs2 value"),
-    new_binary(OP_UDIV, SID_MACHINE_WORD,
-      rs1_value_nid,
-      rs2_value_nid,
-      "rs1 value / rs2 value (unsigned)"),
-    new_binary(OP_UREM, SID_MACHINE_WORD,
-      rs1_value_nid,
-      rs2_value_nid,
-      "rs1 value % rs2 value (unsigned)"),
     "register data flow",
     rd_value_nid,
-    rd_value_nid,
+    decode_RV32M(SID_MACHINE_WORD, ir_nid,
+      new_binary(OP_MUL, SID_MACHINE_WORD,
+        rs1_value_nid,
+        rs2_value_nid,
+        "rs1 value * rs2 value"),
+      new_slice(SID_MACHINE_WORD,
+        new_binary(OP_MUL, SID_DOUBLE_MACHINE_WORD,
+          new_ext(OP_SEXT, SID_DOUBLE_MACHINE_WORD,
+            rs1_value_nid, WORDSIZEINBITS, "sign-extend rs1 value to double machine word"),
+          new_ext(OP_SEXT, SID_DOUBLE_MACHINE_WORD,
+            rs2_value_nid, WORDSIZEINBITS, "sign-extend rs2 value to double machine word"),
+          "double precision rs1 value * rs2 value"),
+        2 * WORDSIZEINBITS - 1,
+        WORDSIZEINBITS,
+        "upper machine word"),
+      new_slice(SID_MACHINE_WORD,
+        new_binary(OP_MUL, SID_DOUBLE_MACHINE_WORD,
+          new_ext(OP_SEXT, SID_DOUBLE_MACHINE_WORD,
+            rs1_value_nid, WORDSIZEINBITS, "sign-extend rs1 value to double machine word"),
+          new_ext(OP_UEXT, SID_DOUBLE_MACHINE_WORD,
+            rs2_value_nid, WORDSIZEINBITS, "unsigned-extend rs2 value to double machine word"),
+          "double precision rs1 value * rs2 value"),
+        2 * WORDSIZEINBITS - 1,
+        WORDSIZEINBITS,
+        "upper machine word"),
+      new_slice(SID_MACHINE_WORD,
+        new_binary(OP_MUL, SID_DOUBLE_MACHINE_WORD,
+          new_ext(OP_UEXT, SID_DOUBLE_MACHINE_WORD,
+            rs1_value_nid, WORDSIZEINBITS, "unsigned-extend rs1 value to double machine word"),
+          new_ext(OP_UEXT, SID_DOUBLE_MACHINE_WORD,
+            rs2_value_nid, WORDSIZEINBITS, "unsigned-extend rs2 value to double machine word"),
+          "double precision rs1 value * rs2 value"),
+        2 * WORDSIZEINBITS - 1,
+        WORDSIZEINBITS,
+        "upper machine word"),
+      new_binary(OP_SDIV, SID_MACHINE_WORD,
+        rs1_value_nid,
+        rs2_value_nid,
+        "rs1 value / rs2 value"),
+      new_binary(OP_UDIV, SID_MACHINE_WORD,
+        rs1_value_nid,
+        rs2_value_nid,
+        "rs1 value / rs2 value (unsigned)"),
+      new_binary(OP_SREM, SID_MACHINE_WORD,
+        rs1_value_nid,
+        rs2_value_nid,
+        "rs1 value % rs2 value"),
+      new_binary(OP_UREM, SID_MACHINE_WORD,
+        rs1_value_nid,
+        rs2_value_nid,
+        "rs1 value % rs2 value (unsigned)"),
+      "RV32M register data flow",
+      rd_value_nid,
+      rd_value_nid),
     other_data_flow_nid);
 }
 
@@ -4235,32 +4327,30 @@ void rotor() {
     "stack-seg-fault",
     "stack segmentation fault");
 
-  division_by_zero_nid = state_property(
-    UNUSED,
-    new_binary_boolean(OP_AND,
-      decode_op(SID_BOOLEAN, ir_nid,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_FALSE,
-        NID_DIVU,
-        NID_REMU,
-        "or just divu or remu?", NID_FALSE, NID_FALSE,
-        NID_FALSE),
-      new_binary_boolean(OP_EQ,
-        load_register_value(get_instruction_rs2(ir_nid), "rs2 value"),
-        NID_MACHINE_WORD_0,
-        "rs2 value == zero?"),
-      "divu or remu by zero?"),
-    "division-by-zero",
-    "division by zero");
+  if (RV32M)
+    division_by_zero_nid = state_property(
+      UNUSED,
+      decode_opcode(SID_BOOLEAN, ir_nid,
+        NID_OP_OP, "OP?",
+        new_binary_boolean(OP_AND,
+          decode_RV32M(SID_BOOLEAN, ir_nid,
+            NID_FALSE,
+            NID_FALSE,
+            NID_FALSE,
+            NID_FALSE,
+            NID_DIV,
+            NID_DIVU,
+            NID_REM,
+            NID_REMU,
+            "or just div or divu or rem or remu?", NID_FALSE, NID_FALSE),
+          new_binary_boolean(OP_EQ,
+            load_register_value(get_instruction_rs2(ir_nid), "rs2 value"),
+            NID_MACHINE_WORD_0,
+            "rs2 value == zero?"),
+          "div or divu or rem or remu by zero?"),
+        "division or remainder by zero?", NID_FALSE),
+      "division-by-zero",
+      "division by zero");
 
   exclude_a0_from_rd_nid = state_property(
     new_binary_boolean(OP_NEQ,
@@ -4394,9 +4484,11 @@ void output_model() {
 
   print_line(stack_seg_faulting_nid);
 
-  print_break("\n");
+  if (RV32M) {
+    print_break("\n");
 
-  print_line(division_by_zero_nid);
+    print_line(division_by_zero_nid);
+  }
 
   //print_break("\n");
 
