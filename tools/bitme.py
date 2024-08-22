1256eb2450d4e135aa962e66d7371ee38a7ff72a
Christoph Kirsch <ckirsch@gmail.com>
2024-08-22 19:52:55 +0200

    Parsing sort lines done

--- tools/bitme.py
+++ tools/bitme.py
@@ -11,25 +11,47 @@ def syntax_error(expected, line_no):
     print("syntax error in line %u: expected %s" % (line_no, expected))
     return False
 
-def parse_sort(tokens, nid, line_no):
+def get_token(tokens, expected, line_no):
     try:
-        token = tokens[1]
+        return tokens[1]
     except:
-        return syntax_error("bitvec or array", line_no)
-    if token == 'bitvec':
-        try:
-            token = tokens[2]
-        except:
-            return syntax_error("size", line_no)
-        if token.isdecimal():
-            size = int(token)
-            print("%u sort bitvec %u" % (nid, size))
+        return syntax_error(expected, line_no)
+
+def parse_sort(tokens, nid, line_no):
+    token = get_token(tokens, "bitvec or array", line_no)
+    if token != False:
+        if token == 'bitvec':
+            token = get_token(tokens[1:], "size", line_no)
+            if token != False:
+                if token.isdecimal():
+                    size = int(token)
+                    print("%u sort bitvec %u" % (nid, size))
+                else:
+                    return syntax_error("size", line_no)
+            else:
+                return False
+        elif token == 'array':
+            token = get_token(tokens[1:], "array size", line_no)
+            if token != False:
+                if token.isdecimal():
+                    array_size = int(token)
+                    token = get_token(tokens[2:], "element size", line_no)
+                    if token != False:
+                        if token.isdecimal():
+                            element_size = int(token)
+                            print("%u sort array %u %u" % (nid, array_size, element_size))
+                        else:
+                            return syntax_error("element size", line_no)
+                    else:
+                        return False
+                else:
+                    return syntax_error("array size", line_no)
+            else:
+                return False
         else:
-            return syntax_error("size", line_no)
-    elif token == 'array':
-        return True
+            return False
     else:
-        return syntax_error("bitvec or array", line_no)
+        return False
     return True
 
 def parse_btor2(line, line_no):
@@ -39,12 +61,12 @@ def parse_btor2(line, line_no):
         if token[0] != ';':
             if token.isdecimal():
                 nid = int(token)
-                try:
-                    token = tokens[1]
-                except:
-                    return syntax_error("keyword", line_no)
-                if token == 'sort':
-                    return parse_sort(tokens[1:], nid, line_no)
+                token = get_token(tokens, "keyword", line_no)
+                if token != False:
+                    if token == 'sort':
+                        return parse_sort(tokens[1:], nid, line_no)
+                else:
+                    return False
             else:
                 return syntax_error("nid", line_no)
     return True
