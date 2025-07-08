c838a8c0d975a62e519234088acf92bbe3129928
Popax21 <popax@popax21.dev>
2025-07-08 14:01:32 +0200

    Final POPL2026 dataset + LaTeX tabulizor

--- tools/bitme/tabulizor.py
+++ tools/bitme/tabulizor.py
@@ -2,8 +2,6 @@ import csv
 import glob
 import re
 import dataclasses
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 
 @dataclasses.dataclass(frozen=True)
@@ -37,94 +35,83 @@ for f in glob.iglob("bitme-data-*.csv"):
                 )
                 samples.setdefault(r["sample"], dict())[cfg] = res
 
-### FIGURE 1: -propagate / -array vs runtime (per BVDD type)
-
 per_bvdd: dict[str, dict[str, dict[BitmeConfig, BenchmarkResult]]] = dict()
 for sample, r in samples.items():
     for cfg, res in r.items():
-        per_bvdd.setdefault(cfg.bvdd_mode, dict()).setdefault(sample, dict())[cfg] = res
+        per_bvdd.setdefault(
+            f"{cfg.bvdd_mode}{cfg.max_propagate_bits}", dict()
+        ).setdefault(sample, dict())[cfg] = res
 
-BVDD_COLORS = {
-    "BVDD": "green",
-    "ROABVDD": "blue",
-    "CFLOBVDD": "purple",
-}
-
-for bvdd, r in per_bvdd.items():
-    avg_pts: dict[int, list[float]] = dict()
-    for sample, r in r.items():
-        pts = []
-        for cfg, res in r.items():
-            x = cfg.max_propagate_bits
-            y = res.runtime_secs
-
-            pts.append((x, y))
-            avg_pts.setdefault(x, []).append(y)
-
-        pts.sort()
-        plt.plot(
-            [p[0] for p in pts],
-            [p[1] for p in pts],
-            marker="o",
-            label=f"{bvdd} - {sample}",
-            alpha=0.1,
-            color=BVDD_COLORS[bvdd],
-        )
-
-    pts = []
-    for x, ys in avg_pts.items():
-        pts.append((x, sum(ys) / len(ys)))
-
-    pts.sort()
-    plt.plot(
-        [p[0] for p in pts],
-        [p[1] for p in pts],
-        label=f"{bvdd} - MED",
-        color=BVDD_COLORS[bvdd],
-    )
-
-plt.xlabel("-propagate / -array")
-plt.ylabel("Runtime (s)")
-plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter("%.fs"))
-plt.show()
-
-del per_bvdd["BVDD"]
-
-### FIGURE 2: per-sample BVDD comparison (for p8, absolute differences)
-sample_names = [
-    s for s, r in samples.items() if any(res.max_propagate_bits == 8 for res in r)
+BNUM = [
+    "ROABVDD0",
+    "ROABVDD1",
+    "ROABVDD8",
+    "CFLOBVDD8",
 ]
+SAMPLES = list(samples.keys())
 
-for i, (bvdd, r) in enumerate(per_bvdd.items()):
-    for j, sample in enumerate(sample_names):
-        for cfg, res in r[sample].items():
-            if cfg.max_propagate_bits == 8:
-                plt.bar(
-                    [j + i * 0.25],
-                    [res.runtime_secs],
-                    0.25,
-                    label=bvdd,
-                    color=BVDD_COLORS[bvdd],
-                )
+print("overview:")
+for idx, bvdd in enumerate(BNUM):
+    print("\\addplot[point meta=explicit,scatter,only marks,mark=*] coordinates {", end="")
+    for sample, r in per_bvdd[bvdd].items():
+        if sample.startswith("cflobvdd"):
+            continue
+        for cfg, res in r.items():
+            if cfg.max_array_flatten_bits == (0 if bvdd[-1] in ["0", "1"] else 8):
+                print(f"({idx+1},{res.runtime_secs}) [{SAMPLES.index(sample)}]", end=" ")
+                break
+    print("};")
 
-plt.ylabel("Runtime (s)")
-plt.xticks(range(len(sample_names)), sample_names, rotation=-20)
-plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter("%.fs"))
-plt.show()
+print()
 
-### FIGURE 3: per-sample BVDD comparison (for p8, absolute differences)
+for bvdd in BNUM:
+    print(f"per-array {bvdd}:")
+    for sample, r in per_bvdd[bvdd].items():
+        if sample.startswith("cflobvdd"):
+            continue
+        per_arr: dict[int, float] = dict()
+        for cfg, res in r.items():
+            per_arr[cfg.max_array_flatten_bits] = res.runtime_secs
+        if not per_arr:
+            continue
 
-for i, sample in enumerate(sample_names):
-    bvdd_vals: dict[str, float] = dict()
-    for cfg, res in samples[sample].items():
-        if cfg.max_propagate_bits == 8:
-            bvdd_vals[cfg.bvdd_mode] = res.runtime_secs
+        print("\\addplot[point meta=explicit,scatter,mark=*] coordinates {", end="")
+        for a in range(0, 8 + 1):
+            if a in per_arr:
+                print(f"({a},{per_arr[a]}) [{SAMPLES.index(sample)}]", end=" ")
+        print("};")
+    print()
 
-    assert "ROABVDD" in bvdd_vals and "CFLOBVDD" in bvdd_vals
-    plt.bar([i], [bvdd_vals["CFLOBVDD"] / bvdd_vals["ROABVDD"] * 100], 0.5)
 
-plt.axhline(y=100, linestyle="--")
-plt.ylabel("Runtime rel. diff. ROABVDD / CFLOBVDD")
-plt.xticks(range(len(sample_names)), sample_names, rotation=-20)
-plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter("%.f%%"))
-plt.show()
+def cflobvdd_plot(name, metric):
+    print()
+    print(f"{name} ({metric}):")
+
+    for col, bvdd in [
+        ("red", "ROABVDD8"),
+        ("blue", "CFLOBVDD8"),
+    ]:
+        per_v: dict[int, float] = dict()
+        for sample, r in per_bvdd[bvdd].items():
+            if not sample.startswith(name):
+                continue
+            for cfg, res in r.items():
+                if cfg.max_array_flatten_bits == 8:
+                    if metric == "runtime":
+                        per_v[int(sample[-3])] = res.runtime_secs
+                    elif metric == "memory":
+                        per_v[int(sample[-3])] = res.memory_usage_kb / 1024
+                    else:
+                        assert False
+
+        print(f"\\addplot[color={col},mark=*] coordinates {{", end="")
+        for a in range(0, 8 + 1):
+            if a in per_v:
+                print(f"({a},{per_v[a]})", end=" ")
+        print("};")
+
+
+cflobvdd_plot("cflobvdd-multi-input", "runtime")
+cflobvdd_plot("cflobvdd-multi-input", "memory")
+cflobvdd_plot("cflobvdd-bit-inversion", "runtime")
+cflobvdd_plot("cflobvdd-bit-inversion", "memory")
