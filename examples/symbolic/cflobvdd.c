4eb8448707d0d54e96809b5da2b9692c833cd6e0
Christoph Kirsch <ckirsch@gmail.com>
2025-09-04 10:00:03 +0200

    cflobvdd C code with additional data flow to exercise BDDs

--- examples/symbolic/cflobvdd.c
+++ examples/symbolic/cflobvdd.c
@@ -1,7 +1,9 @@
 uint64_t main() {
+  uint64_t* x;
   uint64_t  a;
   uint64_t  b;
-  uint64_t* x;
+  uint64_t  x1;
+  uint64_t  x2;
 
   x = malloc(sizeof(uint64_t));
 
@@ -12,8 +14,11 @@ uint64_t main() {
 
   while (a < 2) {
     read(0, x, 1);
+    x1 = *x;
+    read(0, x, 1);
+    x2 = *x;
 
-    if (*x == 48)
+    if (x1 * x2 == 42)
        b = b + 1;
 
     a = a + 1;
