698b21206428ace2f2455f9175f458a7c01fe14d
Christoph Kirsch <ckirsch@gmail.com>
2025-09-04 11:22:15 +0200

    cflobvdd.c with single-digit console argument for controlling input size

--- examples/symbolic/cflobvdd.c
+++ examples/symbolic/cflobvdd.c
@@ -1,9 +1,14 @@
-uint64_t main() {
+uint64_t main(uint64_t argc, uint64_t* argv) {
+  uint64_t  n;
   uint64_t* x;
   uint64_t  a;
   uint64_t  b;
   uint64_t  x1;
   uint64_t  x2;
+
+  // asserting one single-digit console argument
+  n = *((uint64_t*) *(argv + 1)) % 256 - '0';
+  // or else initialize n to some constant >= 0
 
   x = malloc(sizeof(uint64_t));
 
@@ -12,7 +17,7 @@ uint64_t main() {
   a = 0;
   b = 0;
 
-  while (a < 2) {
+  while (a < n) {
     read(0, x, 1);
     x1 = *x;
     read(0, x, 1);
@@ -24,7 +29,7 @@ uint64_t main() {
     a = a + 1;
   }
 
-  if (b == 2)
+  if (b == n)
     return 1;
   else
     return 0;
